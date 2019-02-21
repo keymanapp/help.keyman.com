a5fc05d29a57dd2e975202da0b8cddb7f27ed4fe
Darcy Wong <darcy_wong@sil.org>
2019-02-21 13:32:13 +0700

    Update mac documentation

--- products/mac/11.0/docs/start_download-install_keyman.php
+++ products/mac/11.0/docs/start_download-install_keyman.php
@@ -10,28 +10,42 @@
 <h2>How To - Install Keyman for macOS</h2>
 
 <p>
-	<i>Keyman for macOS</i> is available for <a class="ulink" href="https://keyman.com/mac/download.php" target="_blank">download</a> as a standard Mac disk image (.dmg) file. It should be installed according to the following instructions.
+	<i>Keyman for macOS</i> is available for <a class="ulink" href="https://keyman.com/mac/download.php" target="_blank">download</a> as a standard Apple Disk Image (.dmg) file. It should be installed according to the following instructions.
 </p>
 <ul>
 	<li>
 		Double click the DMG file and drag and drop the Keyman app into the ‘Input Methods’ folder. 
 	</li>
-	<li>
-		Go to "System Preferences", then "Keyboard" and select the "Input Sources" tab. 
-		Or, click on the input menu (usually a country flag or language icon) that appears on the right hand side of the menu bar, 
-		then select "Open Keyboard Preferences". (This step may differ slightly depending on the OS version.)
-	</li>
-	<li>
-		Click on "+", then find Keyman under Multi-Language and double click that entry to add as an input method.
-	</li>
-	<li>
-		Click the input menu and switch to Keyman. Click the input menu again to see the installed keyboards in Keyman:Keyboards menu. 
-		You may also switch to another keyboard from this menu. 
-	</li>
-	<li>
-		Now you can start typing with the selected keyboard in any text editor or application that uses standard OS X input methods. 
-	</li>
+    <li>
+        Add Keyman for macOS as an input source (See <a href="./start_tutorial.php">Getting Started with Keyman for macOS</a> for more details.)<p>
+        <ul>
+            <li>
+                On the Apple menu, click "System Preferences"
+            </li>
+            <li>
+                Click "Keyboard", select the "Input Sources" tab, and then click "+". (This step may differ slightly depending on the OS version.)
+            </li>
+            <li>
+                Select "Multiple Languages", and then select "Keyman" in the right-hand pane to add as an input method.
+            </li>
+            <li>
+                Click the input menu and switch to Keyman. Click the input menu again to see the installed keyboards in Keyman:Keyboards menu.
+                This will show "(None)" at first. You may switch to another keyboard from this menu, once they are installed.<br/>
+                See <a href="./start_download-install_keyboard.php">How To - Download and Install a Keyman Keyboard</a>
+            </li>
+            <li>
+                Now you can start typing with the selected keyboard in any text editor or application that uses standard macOS input methods.
+            </li>
+        </ul>
+    </li>
 </ul>
+
+<h2>How To - Updating Keyman for macOS</h2>
+<ul>
+    <li>First, logout and login again. This ensures that Keyman is not in use.</li>
+    <li>Then you should be able to drag and update Keyman following the installation steps above</li>
+</ul>
+
 <h2>Note</h2>
 <p>
 	To uninstall Keyman for macOS, follow the steps in <a class="xref" href="help_uninstall_keyman.php" title="Help - Uninstall Keyman for macOS">Help - Uninstall Keyman for macOS</a>.
