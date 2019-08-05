455412f2f75835d984d5d71886565d57eeaa2bb2
Keyman Build Server <keyman-server@users.noreply.github.com>
2019-08-05 01:15:52 -0400

    Keyman Desktop help deployment (automatic)

---
+++ products/desktop/13.0/docs/common_unicode.php
@@ -0,0 +1,8 @@
+<?php
+  require_once('includes/template.php');
+
+  head([
+    'title' => "Does Keyman Desktop support Unicode?"
+  ]);
+?>
+<div class="navheader"><table width="100%" summary="Navigation header"><tr><th colspan="3" align="center">Does Keyman Desktop support Unicode?</th></tr><tr><td width="20%" align="left"><a accesskey="p" href="common_osk.php">Prev</a> </td><th width="60%" align="center">Chapter 5. Common Questions</th><td width="20%" align="right"> <a accesskey="n" href="common_compatibility.php">Next</a></td></tr></table><hr></div><div class="section" lang="en"><div class="titlepage"><div><div><h2 class="title" style="clear: both" id="common_unicode">Does Keyman Desktop support Unicode?</h2></div></div></div><p>Yes, since Keyman Desktop 5, Keyman fully supports the Unicode Standard, up to and including Unicode 10.0. We also update Keyman Desktop with each new release of the Unicode Standard.</p></div><div class="navfooter"><hr><table width="100%" summary="Navigation footer"><tr><td width="40%" align="left"><a accesskey="p" href="common_osk.php">Prev</a> </td><td width="20%" align="center"><a accesskey="u" href="common.php">Up</a></td><td width="40%" align="right"> <a accesskey="n" href="common_compatibility.php">Next</a></td></tr><tr><td width="40%" align="left" valign="top">Does Keyman include a virtual on-screen keyboard? </td><td width="20%" align="center"><a accesskey="h" href="index.php">Home</a></td><td width="40%" align="right" valign="top"> Does Keyman work with all software?</td></tr></table></div>
