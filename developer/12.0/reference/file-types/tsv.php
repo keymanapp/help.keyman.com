4c6de078cce03708a3dd579a5cc8ae8f6fc78e83
Eddie Antonio Santos <easantos@ualberta.ca>
2019-08-12 13:50:14 -0600

    Update TSV information from the latest beta release.

--- developer/12.0/reference/file-types/tsv.php
+++ developer/12.0/reference/file-types/tsv.php
@@ -28,18 +28,30 @@
 
 <p>The <strong>lexical model compiler</strong> expects word lists to abide by the following <strong>tab-separated values</strong> (TSV) format:</p>
 <ul>
-<li>the file must be <strong>UTF-8</strong> encoded text file</li>
+<li>the file is a <strong>UTF-8</strong> encoded text file</li>
 <li>newlines are either <strong>LF</strong> or <strong>CRLF</strong></li>
+<li>the file <strong>MAY</strong> start with the UTF-8 <strong>byte-order mark</strong>
+  (<abbr>BOM</abbrv>); that is, if the first three bytes of the file are
+  <code>EF BB BF</code>, these will be interpreted as the
+  <abbr>BOM</abbr> and will be ignored.</li>
 <li>the file either consists of a <strong>comment</strong> or an <strong>entry</strong></li>
 <li><strong>comment</strong> lines MUST start with the <code>#</code> character on the very first column</li>
-<li><strong>entries</strong> are one or two columns, separated by the (horizontal) <strong>tab character</strong> (U+0009)</li>
-<li>column 1 (REQUIRED): the <strong>word form</strong>: can have any character except tab, CR, or LF. Surrounding whitespace characters are trimmed. Quote characters (<code>'</code> or <code>"</code>) are <strong>NOT</strong> required to surround the text and are <strong>NOT</strong> parsed in any special manner.</li>
-<li>column 2 (optional): the <strong>count</strong>: a non-negative integer specifying how many times this entry has appeared in the corpus. Blank means ‘indeterminate’, and is treated as if the word exists in the corpus, but will be predicted at the lowest possible priority.</li>
-<li>any other columns are ignored.</li>
+<li><strong>entries</strong> are one to three columns, separated by the (horizontal) <strong>tab character</strong> (U+0009)</li>
+<li>column 1 (<strong>REQUIRED</strong>): the <strong>word form</strong>: can
+  have any character except tab, CR, or LF. Surrounding whitespace characters
+  are trimmed. Quote characters (<code>'</code> or <code>"</code>) are
+  <strong>NOT</strong> required to surround the text and are
+  <strong>NOT</strong> parsed in any special manner.</li>
+<li>column 2 (<i>optional</i>): the <strong>count</strong>: a non-negative integer
+  specifying how many times this entry has appeared in the corpus. Blank means
+  ‘indeterminate’, and is treated as if the word exists in the corpus, but will
+  be predicted at the lowest possible priority.</li>
+<li>column 3 (<i>optional</i>): <strong>comment</strong>: an informative
+  comment, ignored by this tool.</li>
 </ul>
 
 <aside>
-<p>Source: <cite><a href="https://github.com/keymanapp/keyman/blob/307436e7c24caa5c720b272640184362c4dc3223/developer/js/lexical-model-compiler/build-trie.ts#L21-L66">build-trie.js@e58173f</a></cite></p>
+<p>Source: <cite><a href="https://github.com/keymanapp/keyman/blob/029fb7c822c5a5619eaca845ecd2e5a2497d3056/developer/js/lexical-model-compiler/build-trie.ts#L21-L40">build-trie.js@029fb7c8</a></cite></p>
 </aside>
 
 <h2>Additional notes</h2>
