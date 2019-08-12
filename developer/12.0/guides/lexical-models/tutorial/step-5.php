da255dbd36568f2923266633225103b96b81a2af
Eddie Antonio Santos <easantos@ualberta.ca>
2019-08-12 14:38:40 -0600

    Convert code examples to correct markup.

--- developer/12.0/guides/lexical-models/tutorial/step-5.php
+++ developer/12.0/guides/lexical-models/tutorial/step-5.php
@@ -73,10 +73,10 @@ mkdir -p example/str.tutorial/source</code></pre>
 
 <p>The <code>.model_info</code> file should follow this template:</p>
 
-<div class="sourceCode" id="cb9"><pre class="sourceCode json"><code class="sourceCode json"><span id="cb9-1"><a href="#cb9-1"></a><span class="fu">{</span></span>
-<span id="cb9-2"><a href="#cb9-2"></a>    <span class="dt">&quot;license&quot;</span><span class="fu">:</span> <span class="st">&quot;&lt;SPDX license identifier&gt;&quot;</span><span class="fu">,</span></span>
-<span id="cb9-3"><a href="#cb9-3"></a>    <span class="dt">&quot;description&quot;</span><span class="fu">:</span> <span class="st">&quot;&lt;short description of the lexical model&gt;&quot;</span></span>
-<span id="cb9-4"><a href="#cb9-4"></a><span class="fu">}</span></span></code></pre></div>
+<pre class="language-js"><code>{
+    "license": "&lt;SPDX license identifier&gt;",
+    "description": "&lt;short description of the lexical model&gt;"
+}</code></pre>
 
 <p>Copy this template, and use it to create your own <code>.model_info</code> file.</p>
 
@@ -88,10 +88,10 @@ mkdir -p example/str.tutorial/source</code></pre>
 
 <p>In our <code>example.str.tutorial</code> example, I’ll use the following <code>.model_info</code>.</p>
 
-<div class="sourceCode" id="cb10"><pre class="sourceCode json"><code class="sourceCode json"><span id="cb10-1"><a href="#cb10-1"></a><span class="fu">{</span></span>
-<span id="cb10-2"><a href="#cb10-2"></a>    <span class="dt">&quot;license&quot;</span><span class="fu">:</span> <span class="st">&quot;mit&quot;</span><span class="fu">,</span></span>
-<span id="cb10-3"><a href="#cb10-3"></a>    <span class="dt">&quot;description&quot;</span><span class="fu">:</span> <span class="st">&quot;Predicts words in the SENĆOŦEN language (Saanich Dialect)&quot;</span></span>
-<span id="cb10-4"><a href="#cb10-4"></a><span class="fu">}</span></span></code></pre></div>
+<pre class="language-js"><code>{
+    "license": "mit",
+    "description": "Predicts words in the SENĆOŦEN language (Saanich Dialect)"
+}</code></pre>
 
 <p>I’ll save this file to <code>example/str.tutorial/example.str.tutorial.model_info</code>.</p>
 
@@ -100,53 +100,53 @@ mkdir -p example/str.tutorial/source</code></pre>
 
 <p><strong>TODO:</strong></p>
 
-<div class="sourceCode" id="cb11"><pre class="sourceCode xml"><code class="sourceCode xml"><span id="cb11-1"><a href="#cb11-1"></a><span class="kw">&lt;?xml</span> version=&quot;1.0&quot; encoding=&quot;utf-8&quot;<span class="kw">?&gt;</span></span>
-<span id="cb11-2"><a href="#cb11-2"></a><span class="kw">&lt;Package&gt;</span></span>
-<span id="cb11-3"><a href="#cb11-3"></a>  <span class="kw">&lt;System&gt;</span></span>
-<span id="cb11-4"><a href="#cb11-4"></a>    <span class="kw">&lt;KeymanDeveloperVersion&gt;</span>12.0.1500.0<span class="kw">&lt;/KeymanDeveloperVersion&gt;</span></span>
-<span id="cb11-5"><a href="#cb11-5"></a>    <span class="kw">&lt;FileVersion&gt;</span>12.0<span class="kw">&lt;/FileVersion&gt;</span></span>
-<span id="cb11-6"><a href="#cb11-6"></a>  <span class="kw">&lt;/System&gt;</span></span>
-<span id="cb11-7"><a href="#cb11-7"></a>  <span class="kw">&lt;Options&gt;</span></span>
-<span id="cb11-8"><a href="#cb11-8"></a>    <span class="kw">&lt;FollowKeyboardVersion/&gt;</span></span>
-<span id="cb11-9"><a href="#cb11-9"></a>  <span class="kw">&lt;/Options&gt;</span></span>
-<span id="cb11-10"><a href="#cb11-10"></a>  <span class="kw">&lt;Info&gt;</span></span>
-<span id="cb11-11"><a href="#cb11-11"></a>    <span class="kw">&lt;Name</span><span class="ot"> URL=</span><span class="st">&quot;&quot;</span><span class="kw">&gt;</span>SENĆOŦEN (Saanich Dialect) Lexical Model<span class="kw">&lt;/Name&gt;</span></span>
-<span id="cb11-12"><a href="#cb11-12"></a>    <span class="kw">&lt;Copyright</span><span class="ot"> URL=</span><span class="st">&quot;&quot;</span><span class="kw">&gt;</span>© 2019 National Research Council Canada<span class="kw">&lt;/Copyright&gt;</span></span>
-<span id="cb11-13"><a href="#cb11-13"></a>    <span class="kw">&lt;Author</span><span class="ot"> URL=</span><span class="st">&quot;mailto:Eddie.Santos@nrc-cnrc.gc.ca&quot;</span><span class="kw">&gt;</span>Eddie Antonio Santos<span class="kw">&lt;/Author&gt;</span></span>
-<span id="cb11-14"><a href="#cb11-14"></a>    <span class="kw">&lt;Version&gt;</span>1.0.3<span class="kw">&lt;/Version&gt;</span></span>
-<span id="cb11-15"><a href="#cb11-15"></a>  <span class="kw">&lt;/Info&gt;</span></span>
-<span id="cb11-16"><a href="#cb11-16"></a>  <span class="kw">&lt;Files&gt;</span></span>
-<span id="cb11-17"><a href="#cb11-17"></a>    <span class="kw">&lt;File&gt;</span></span>
-<span id="cb11-18"><a href="#cb11-18"></a>      <span class="kw">&lt;Name&gt;</span>..\build\nrc.str.sencoten.model.js<span class="kw">&lt;/Name&gt;</span></span>
-<span id="cb11-19"><a href="#cb11-19"></a>      <span class="kw">&lt;Description&gt;</span>Lexical model nrc.str.sencoten.model.js<span class="kw">&lt;/Description&gt;</span></span>
-<span id="cb11-20"><a href="#cb11-20"></a>      <span class="kw">&lt;CopyLocation&gt;</span>0<span class="kw">&lt;/CopyLocation&gt;</span></span>
-<span id="cb11-21"><a href="#cb11-21"></a>      <span class="kw">&lt;FileType&gt;</span>.model.js<span class="kw">&lt;/FileType&gt;</span></span>
-<span id="cb11-22"><a href="#cb11-22"></a>    <span class="kw">&lt;/File&gt;</span></span>
-<span id="cb11-23"><a href="#cb11-23"></a>  <span class="kw">&lt;/Files&gt;</span></span>
-<span id="cb11-24"><a href="#cb11-24"></a>  <span class="kw">&lt;LexicalModels&gt;</span></span>
-<span id="cb11-25"><a href="#cb11-25"></a>    <span class="kw">&lt;LexicalModel&gt;</span></span>
-<span id="cb11-26"><a href="#cb11-26"></a>      <span class="kw">&lt;Name&gt;</span>SENĆOŦEN dictionary<span class="kw">&lt;/Name&gt;</span></span>
-<span id="cb11-27"><a href="#cb11-27"></a>      <span class="kw">&lt;ID&gt;</span>nrc.str.sencoten<span class="kw">&lt;/ID&gt;</span></span>
-<span id="cb11-28"><a href="#cb11-28"></a>      <span class="kw">&lt;Version&gt;</span>1.0.3<span class="kw">&lt;/Version&gt;</span></span>
-<span id="cb11-29"><a href="#cb11-29"></a>      <span class="kw">&lt;Languages&gt;</span></span>
-<span id="cb11-30"><a href="#cb11-30"></a>        <span class="kw">&lt;Language</span><span class="ot"> ID=</span><span class="st">&quot;str&quot;</span><span class="kw">&gt;</span>North Straits Salish<span class="kw">&lt;/Language&gt;</span></span>
-<span id="cb11-31"><a href="#cb11-31"></a>        <span class="kw">&lt;Language</span><span class="ot"> ID=</span><span class="st">&quot;str-Latn&quot;</span><span class="kw">&gt;</span>SENĆOŦEN<span class="kw">&lt;/Language&gt;</span></span>
-<span id="cb11-32"><a href="#cb11-32"></a>      <span class="kw">&lt;/Languages&gt;</span></span>
-<span id="cb11-33"><a href="#cb11-33"></a>    <span class="kw">&lt;/LexicalModel&gt;</span></span>
-<span id="cb11-34"><a href="#cb11-34"></a>  <span class="kw">&lt;/LexicalModels&gt;</span></span>
-<span id="cb11-35"><a href="#cb11-35"></a><span class="kw">&lt;/Package&gt;</span></span></code></pre></div>
+<pre class="language-xml"><code>&lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-8&quot;?&gt;
+&lt;Package&gt;
+  &lt;System&gt;
+    &lt;KeymanDeveloperVersion&gt;12.0.1500.0&lt;/KeymanDeveloperVersion&gt;
+    &lt;FileVersion&gt;12.0&lt;/FileVersion&gt;
+  &lt;/System&gt;
+  &lt;Options&gt;
+    &lt;FollowKeyboardVersion/&gt;
+  &lt;/Options&gt;
+  &lt;Info&gt;
+    &lt;Name URL=&quot;&quot;&gt;SENĆOŦEN (Saanich Dialect) Lexical Model&lt;/Name&gt;
+    &lt;Copyright URL=&quot;&quot;&gt;© 2019 National Research Council Canada&lt;/Copyright&gt;
+    &lt;Author URL=&quot;mailto:Eddie.Santos@nrc-cnrc.gc.ca&quot;&gt;Eddie Antonio Santos&lt;/Author&gt;
+    &lt;Version&gt;1.0.3&lt;/Version&gt;
+  &lt;/Info&gt;
+  &lt;Files&gt;
+    &lt;File&gt;
+      &lt;Name&gt;..\build\nrc.str.sencoten.model.js&lt;/Name&gt;
+      &lt;Description&gt;Lexical model nrc.str.sencoten.model.js&lt;/Description&gt;
+      &lt;CopyLocation&gt;0&lt;/CopyLocation&gt;
+      &lt;FileType&gt;.model.js&lt;/FileType&gt;
+    &lt;/File&gt;
+  &lt;/Files&gt;
+  &lt;LexicalModels&gt;
+    &lt;LexicalModel&gt;
+      &lt;Name&gt;SENĆOŦEN dictionary&lt;/Name&gt;
+      &lt;ID&gt;nrc.str.sencoten&lt;/ID&gt;
+      &lt;Version&gt;1.0.3&lt;/Version&gt;
+      &lt;Languages&gt;
+        &lt;Language ID=&quot;str&quot;&gt;North Straits Salish&lt;/Language&gt;
+        &lt;Language ID=&quot;str-Latn&quot;&gt;SENĆOŦEN&lt;/Language&gt;
+      &lt;/Languages&gt;
+    &lt;/LexicalModel&gt;
+  &lt;/LexicalModels&gt;
+&lt;/Package&gt;</code></pre>
 
 <h2 id="step-4.3-creating-the-model-file">Step 4.3: Creating the model file</h2>
 
 <p>Finally, the <code>model.ts</code> is a TypeScript source code file that is executed to build the model. For our purposes, we can simply copy-paste the following example:</p>
 
-<div class="sourceCode" id="cb12"><pre class="sourceCode typescript"><code class="sourceCode typescript"><span id="cb12-1"><a href="#cb12-1"></a><span class="im">import</span> LexicalModelCompiler <span class="im">from</span> <span class="st">&quot;@keymanapp/developer-lexical-model-compiler&quot;</span><span class="op">;</span></span>
-<span id="cb12-2"><a href="#cb12-2"></a></span>
-<span id="cb12-3"><a href="#cb12-3"></a>(<span class="kw">new</span> LexicalModelCompiler).<span class="fu">compile</span>(<span class="op">{</span></span>
-<span id="cb12-4"><a href="#cb12-4"></a>  format<span class="op">:</span> <span class="st">&#39;trie-1.0&#39;</span><span class="op">,</span></span>
-<span id="cb12-5"><a href="#cb12-5"></a>  wordBreaking<span class="op">:</span> <span class="st">&#39;default&#39;</span><span class="op">,</span></span>
-<span id="cb12-6"><a href="#cb12-6"></a>  sources<span class="op">:</span> <span class="op">[</span><span class="st">&#39;wordlist.tsv&#39;</span><span class="op">],</span></span>
-<span id="cb12-7"><a href="#cb12-7"></a><span class="op">}</span>)<span class="op">;</span></span></code></pre></div>
+<pre class="language-typescript"><code>import LexicalModelCompiler from &quot;@keymanapp/developer-lexical-model-compiler&quot;;
+
+(new LexicalModelCompiler).compile({
+  format: &apos;trie-1.0&apos;,
+  wordBreaking: &apos;default&apos;,
+  sources: [&apos;wordlist.tsv&apos;],
+});</code></pre>
 
 <p>Most importantly, this file specifies that model will use the word list in <code>wordlist.tsv</code>.</p>
 
