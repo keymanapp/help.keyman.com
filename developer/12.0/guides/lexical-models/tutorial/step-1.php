c03240ff268fa2562eba68fc6f7743af2a28cfc2
Eddie Antonio Santos <easantos@ualberta.ca>
2019-08-27 11:18:26 -0600

    No longer need NodeJS!

--- developer/12.0/guides/lexical-models/tutorial/step-1.php
+++ developer/12.0/guides/lexical-models/tutorial/step-1.php
@@ -8,18 +8,13 @@
 
 <h1>Step 1: Prerequisites for building a lexical model</h1>
 
-<p> To create any kind of lexical model, we will need some software installed. </p>
-
-<h2>Requirements</h2>
-
-<p>You will need to install:</p>
+<p> To prepare a lexical model based on a word list, we will need a few things: </p>
 
 <ul>
-  <li><a href="https://keyman.com/developer">Keyman Developer</a> 12.0 or greater</li>
-  <li><a href="https://nodejs.org/">Node.js</a> 10.0 or greater</li>
+  <li>Install <a href="https://keyman.com/developer">Keyman Developer</a> 12.0 or greater (Windows only)</li>
+  <li> A list of words in your langauge, stored in an spreadsheet, in dictionary-making software or, other means. </li>
 </ul>
 
-<p> Once you have the appropriate software installed, we can start preparing
-your language data! </p>
+<p> Once we have the prerequisite software and data, we can begin to prepare the data! </p>
 
 <p><a href="step-2.php" title="Step 2: Get some language data">Step 2: Get some language data</a></p>
