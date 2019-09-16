4db078bf383790e5f085296c4e1d2057cffc9cc3
Joshua A. Horton <joshua_horton@sil.org>
2019-09-16 14:56:20 +0700

    Reworks 'switching between keyboards'

--- products/iphone-and-ipad/12.0/switching-between-keyboards.php
+++ products/iphone-and-ipad/12.0/switching-between-keyboards.php
@@ -17,35 +17,27 @@ $titlePrefix = $embed_ios ? "" : "Keyman for iPhone and iPad:  ";
 <h2><?=$titlePrefix?>Switching between Keyboards</h2>
 
 <p>
-
-   To switch keyboards, follow these steps:
-   <br/><br/>
-   Step 1)
-   <br/>
-   With the keyboard visible, touch the globe key:
-   <br/>
-   <img src="<?php echo cdn("img/app/12.0/globe.png"); ?>" />
-   <br/>
-   This will bring up a list of all currently installed languages (the default is English USA). If you have already downloaded additional languages, they will appear here. Simply select them and the keyboard will re-appear with the new language.
-   <br/><br/>
-   If you haven't installed any languages as yet, touch the <span class="command">+</span> button in the top right corner of your screen.
-   <br/><br/>
-   Step 2)
-   <br/>
-   A list of all available languages will appear. Scroll through this list until you find the language you want to install. Some languages may have multiple keyboards - for example we have 10 different Tamil keyboards.
-   <br/>
-   <img src="<?php echo cdn("img/app/12.0/lang-list.png"); ?>" />
-   <br/><br/>
-   Select the language and keyboard you want to install - a popup will ask for confirmation, hit the <span class="command">Download</span> button.
-   <br/>
-   <img src="<?php echo cdn("img/app/12.0/confirm-dl.png"); ?>" />
-   <br/><br/>
-   Step 3)
-   <br/>
-   Once the download is complete, the new keyboard will be selected. Touch the <span class="command">Done</span> button on the top right corner of your screen and the new keyboard will appear.
-   <br/>
-   <img src="<?php echo cdn("img/app/12.0/dl-success.png"); ?>" />
+   Keyboard switching is always activated by using the following key:
 </p>
+ <p>
+   <img src="<?php echo cdn("img/app/12.0/globe.png"); ?>" />
+ </p>
+<p>
+  This will bring up a list of all currently installed languages (the default is English USA). If you have already downloaded additional languages, they will appear here. Simply select them and the keyboard will re-appear with the new language.
+</p>
+<h3>The In-App Menu</h3>
+<p>
+  <img src="<?php echo cdn("img/app/12.0/in-app-picker-i.png"); ?>" />
+</p>
+<p>Simply select any keyboard to activate it and return to the previous screen.</p>
+<h3>The System Keyboard Menu</h3>
+<p>
+  <img src="<?php echo cdn("img/app/12.0/system-picker-i.png"); ?>" />
+</p>
+<p>This menu only displays when the 'globe' key is touched and held.</p>
+<p>The "Close Keyman" option will activate the next available non-Keyman system keyboard.</p>
+<p>A quick tap on the "globe" key will instead transition to the next available installed keyboard
+  without displaying the menu.</p>
 
 <h2>Help Index</h2>
 
@@ -55,10 +47,11 @@ $indexName = $embed_ios ? "Getting Started" : "Keyman for iPhone and iPad help h
 
 <ul>
   <li><a href='index.php<?=$session_query_q?>'><?=$indexName?></a></li>
+  <li><a href='installing-keyboards.php<?=$session_query_q?>'>Installing keyboards</a></li>
   <li><a href='switching-between-keyboards.php<?=$session_query_q?>'>Switching between keyboards</a></li>
   <li><a href='uninstalling-keyboards.php<?=$session_query_q?>'>Uninstalling keyboards</a></li>
+  <li><a href='installing-custom-keyboards.php<?=$session_query_q?>'>Installing custom keyboards</a></li>
   <li><a href='installing-system-keyboard.php<?=$session_query_q?>'>Installing the Keyman System Keyboard</a></li>
   <li><a href='installing-fonts.php<?=$session_query_q?>'>Installing fonts</a></li>
-  <li><a href='installing-custom-keyboards.php<?=$session_query_q?>'>Installing custom keyboards</a></li>
   <li><a href='using-keyman-browser.php<?=$session_query_q?>'>Using the Keyman Browser</a></li>
 </ul>
