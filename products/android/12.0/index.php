2bfcaff84a7344228f3e2eb2d351b47ca6023dff
Darcy Wong <darcy_wong@sil.org>
2019-10-01 16:42:20 +0700

    Consolidate help links into a _shared file

--- products/android/12.0/index.php
+++ products/android/12.0/index.php
@@ -130,18 +130,9 @@ head([
 <p><img id="keyboard-help2-ap" src="<?= cdn("img/app/12.0/info-a-gray.png")?>"></p>
 <br/>
 
-<h2>Help Index</h2>
-
-<ul>
-  <li class="content-in-app"><a href="index.php">Getting Started</a></li>
-  <li class="content-online"><a href="index.php">Keyman for Android help home</a></li>
-  <li><a href="installing-keyboards.php">Adding new keyboards</a></li>
-  <li><a href='switching-between-keyboards.php'>Switching between keyboards</a></li>
-  <li><a href='uninstalling-keyboards.php'>Removing a keyboard</a></li>
-  <li><a href='installing-custom-packages.php'>Installing custom keyboards/dictionaries</a></li>
-  <li><a href='installing-system-keyboard.php'>Installing system-wide keyboards</a></li>
-  <li><a href='using-keyman-browser.php'>Using the Keyman browser</a></li>
-</ul>
+<?php
+include('./_shared/help_links.php');
+?>
 
 <h2>Integrate Keyman with your App or Website</h2>
 <p>
--- products/android/12.0/installing-keyboards.php
+++ products/android/12.0/installing-keyboards.php
@@ -72,15 +72,6 @@ head([
   <img id="close-keyman-a" src="<?= cdn("img/app/12.0/close-keyman-a.png")?> "/>
 </p>
 
-<h2>Help Index</h2>
-
-<ul>
-  <li class="content-in-app"><a href="index.php">Getting Started</a></li>
-  <li class="content-online"><a href="index.php">Keyman for Android help home</a></li>
-  <li><a href="installing-keyboards.php">Adding new keyboards</a></li>
-  <li><a href='switching-between-keyboards.php'>Switching between keyboards</a></li>
-  <li><a href='uninstalling-keyboards.php'>Removing a keyboard</a></li>
-  <li><a href='installing-custom-packages.php'>Installing custom keyboards/dictionaries</a></li>
-  <li><a href='installing-system-keyboard.php'>Installing system-wide keyboards</a></li>
-  <li><a href='using-keyman-browser.php'>Using the Keyman browser</a></li>
-</ul>
+<?php
+include('./_shared/help_links.php');
+?>
