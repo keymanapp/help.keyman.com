93a4fbeef284355c99b6cc570e023fee00f1aff8
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-08 09:59:06 -0600

    Start to revamp 'Creating a lexical model project' tutorial step.

--- developer/12.0/guides/lexical-models/tutorial/step-2.php
+++ developer/12.0/guides/lexical-models/tutorial/step-2.php
@@ -3,48 +3,139 @@
 
   head([
     'title' => "Step 2: Creating a lexical model project",
-    'css' => ['template.css', 'index.css', 'prism.css', 'kmguides.css']
+    'css' => ['template.css', 'index.css', 'prism.css', 'kmguides.css', 'dev-lexical-models.css']
+
   ]);
 ?>
 
 <h1>Step 2: Creating a lexical model project</h1>
 
 <h2>Create the new project</h2>
-<p>Start Keyman Developer. From the Project menu, select "New Project" and then "Wordlist Lexical Model."</p>
 
-<p class='context-main-image'><img src="<?= cdn('img/developer/120/ui/frmNewLMProject.png') ?>" alt="New LM Project dialog" /></p>
+<figure>
+  <img src="<?= cdn('img/developer/120/ui/frmNewLMProject.png') ?>"
+       alt="“New Project” dialog" />
+  <figcaption>The “New Project” dialog, with “Wordlist Lexical Model” selected.</figcaption>
+</figure>
 
-<h2>New Lexical Model Project Parameters</h2>
-<p> To create a lexical model project, we will need the following information: </p>
+<p>Start Keyman Developer. From the Project menu, select “New Project” and then “Wordlist Lexical Model.”</p>
 
-<p class='context-main-image'><img src="<?= cdn('img/developer/120/ui/frmNewLMProjectParameters.png') ?>" alt="New LM Project Parameters" /></p>
+
+<h2>Provide required information</h2>
+
+<figure class="aside">
+  <img src="<?= cdn('img/developer/120/ui/frmNewLMProjectParameters.png') ?>" alt="New LM Project Parameters" />
+  <figcaption>
+   The New Lexical Model dialog box.
+  </figcaption>
+</figure>
+
+<p> To make sharing your lexical model easier, a project needs the following information: </p>
 
 <dl>
-  <dt>Author Name:</dt>
-  <dd>Your name or organization, such as <code>nrc</code> or <code>sil</code>. Required.</dd>
+  <dt>Author Name</dt>
+  <dd>This is either your <strong>full name</strong> or the
+  <strong>organization</strong> you're create a model for. In this example, I
+  am creating a lexical model on behalf of my organization, the National
+  Research Council Canada, so I write that as the author name.</dd>
 
-  <dt>Model Name:</dt>
-  <dd>We recommend the name of the language, dialect, or community that this model is intended for. The name needs
-    to be unique from other lexical models in the language. Do not include a version string in the model name.
-    Here, we use a model name <code>sencoten</code>. Required.</dd>
-
-  <dt>Copyright:</dt>
-  <dd>A copyright string for the model. Optional.</dd>
-
-  <dt>Version:</dt>
-  <dd>A version string made of <code>major revision</code>.<code>minor revision</code>.
-    We recommend 1.0 for the initial version number. Required.</dd>
-
-  <dt>Languages:</dt>
-  <dd>Use the <span class="guibutton">Add</span> button to bring up the "Select BCP 47 Tag" dialog.
-    When Keyman installs this lexical model, it will associate the matching keyboard with the language(s) you select here.
-    In this example, we add <code>str</code> for the Straits Salish (SENĆOŦEN) language. Required</dd>
+  <dt>Model Name</dt>
+  <dd>We recommend the name of the <strong>language</strong>,
+      <strong>dialect</strong>, or <strong>community</strong> that this model is
+      intended for. The name must be written in all lowercase Latin letters or Arabic numerals.
+      In this example, we're creating a language model for SENĆOŦEN, so we use the model name <code>sencoten</code>.</dd>
+  </dd>
 </dl>
 
-<p class="context-main-image"><img src="<?= cdn('img/developer/120/ui/frmNewLMProjectSelectLanguage.png') ?>" alt="Select BCP 47 Tag dialog" /></p>
+<h2> Provide auxiliary information </h2>
+
+<p> The following information is required, but most users will use
+<strong>default values</strong>. </p>
+
+<dl>
+  <dt>Copyright</dt>
+  <dd>Who owns the rights to this model and its data? Typically, you can use
+      this template: <em>&copy; <?= gmdate("Y") ?> <strong>Your Full Name</strong> or
+      <strong> Your Organization</strong></em>. </dd>
+  <dt>Version</dt>
+  <dd>If this is the first time you've created a lexical model for you
+      language, you should leave the version as <strong>1.0</strong>. Otherwise,
+      your version number must conform to the following rules: A version string made of
+      <code>major revision number</code>.<code>minor revision number</code>.
+  </dd>
+</dl>
+
+<h2> Determine your language tag </h2>
+
+<p> Keyman needs to know how to link your model to the appropriate keyboard
+layout, so that they can both start together. To do this, Keyman utilizes
+<strong> BCP 47 </strong> language tags. </p>
+
+<p> To add a language tag, click the <span class="guibutton">Add</span> button
+to bring up the “Select BCP 47 Tag” dialog box.  </p>
+
+<figure class="aside">
+  <img src="<?= cdn('img/developer/120/ui/frmNewLMProjectSelectLanguage.png') ?>" alt="Select BCP 47 Tag dialog" />
+  <figcaption>
+    The “Select BCP 47 Tag” dialog box for SENĆOŦEN.
+  </figcaption>
+</figure>
+
+<p>The only required option is the <dfn>Language tag</dfn>, which is an
+<a href="">ISO 639-3</a> code. ISO 639-3 tags are a three-letter code. </p>
+
+<p>You can use <a href="https://glottolog.org/glottolog/language">Glottolog</a>
+to search for your language, and it will give you an appropriate code. In this
+example, I searched Glottolog for
+“<a href="https://glottolog.org/resource/languoid/id/saan1246">Saanich</a>”
+(name of the First Nations that speak SENĆOŦEN) and found <code>str</code> as
+the code for all Straits Salish languages. </p>
+
+<p>The next two fields are <strong>optional</strong>, however allow you to be more specific
+about your language.</p>
+
+<h3> The Script tag </h3>
+<p> The <dfn>Script tag</dfn> allows you to specify the writing system used in
+your language model. If your language only uses one writing system, <strong>leave this
+blank</strong>.</p>
+
+<p> Otherwise, in cases where a language can be written in many different
+writing systems, you can use this field to choose the <a
+  href="https://en.wikipedia.org/wiki/ISO_15924">ISO 15924</a> script tag that your lexical model produces.
+
+<p>For example, Plains Cree can either be written in
+<em>standard Roman orthography</em>, a <strong>Latin</strong> derived script,
+or it can be written in <em>syllabics</em>, which is part of the
+<strong>Canadian Aboriginal syllabics </strong> family of writing systems. If
+I wrote a model that produced syllabics, I would choose <code>Cans</code>, as
+that is the <strong>ISO 15924</strong> tag for Canadian Aboriginal
+syllabics.</p>
+
+<!-- TODO: Region tag -->
+
+<p> Once you are finished adding the primary language, click <span
+class="guibutton">OK</span> to return to the <strong>New Lexical Model Project dialog</strong>.
+
+
+<h2> The model ID </h2>
+
+<p> Keyman will create a <dfn>model ID</dfn> which is how Keyman sorts
+and organizes different lexical models. If you followed the above steps, the
+model ID will have been created for you.</p> 
+
+<p> If you're satisfied with the automatically generated model ID, you can
+<a href="#finished">skip to the next step</a>. </p>
+
+<p>In this example, my model ID is
+<code>nrc.str.sencoten</code>, derived from my organization name, the name of
+the primary language, and my model name.</p>
+
 
 <p>These identifiers will be combined to make the <code class="language-none">Model ID</code> = <code>Author ID</code>.
   <code class="language-none">Primary Language</code>.<code class="language-none">Unique Name</code></p>
+
+<hr>
+<a id="finished"></a>
 
 <p> Once we have created the project, we can begin to prepare the data! </p>
 
