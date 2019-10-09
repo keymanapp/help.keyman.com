2ed82dcdc3eb70dce84c30526b977cf49bad4264
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-09 10:56:19 -0600

    pupusas y chuchitos

--- developer/12.0/guides/lexical-models/tutorial/step-2.php
+++ developer/12.0/guides/lexical-models/tutorial/step-2.php
@@ -88,6 +88,7 @@ to bring up the “Select BCP 47 Tag” dialog box.  </p>
   </figcaption>
 </figure>
 
+
 <h3> The language subtag </h3>
 
 <p>The only required option is the <dfn>Language subtag</dfn>, which is an
@@ -99,7 +100,7 @@ code.  First, try to find your language on the list of two-letter ISO 639-1
 codes. <a href="https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes">This
 Wikipedia page</a> lists all of the two-letter codes.</p>
 
-<p>If you can't find a two-letter code, you'll need to find the closes three-letter code.
+<p>If you can't find a two-letter code, you'll need to find the closest three-letter code.
 You can use <a href="https://glottolog.org/glottolog/language">Glottolog</a>
 to search for your language, and it will give you an appropriate code. In this
 example, I searched Glottolog for
@@ -107,10 +108,11 @@ example, I searched Glottolog for
 (name of the First Nations that speak SENĆOŦEN) and found <code>str</code> as
 the code for all Straits Salish languages. </p>
 
-<p>The next two fields are <strong>optional</strong>, however allow you to be more specific
+<p>The next two fields are <strong>optional</strong>, however, they allow you to be more specific
 about your language.</p>
 
-<h3> The Script tag </h3>
+<h3> The script subtag </h3>
+
 <p> The <dfn>Script tag</dfn> allows you to specify the writing system used in
 your language model. If your language only uses one writing system, <strong>leave this
 blank</strong>.</p>
@@ -127,7 +129,29 @@ I wrote a model that produced syllabics, I would choose <code>Cans</code>, as
 that is the <strong>ISO 15924</strong> tag for Canadian Aboriginal
 syllabics.</p>
 
-<!-- TODO: Region tag -->
+
+<h3>The region subtag</h3>
+
+<p> The <dfn>Region tag</dfn> allows you to specify the region your language
+or dialect is spoken in. Fill this in only if your language varies between
+different regions. In our example, SENĆOŦEN is specific enough to cover the
+entire W̱SÁNEĆ region, where SENĆOŦEN is spoken, so this field may be left blank.</p>
+
+<p>However, large languages, like English, Spanish, or French have quite
+different vocabulary and even grammar across different regions. For example,
+the Spanish spoken in Spain regularly uses words that are uncommon or even
+vulgar in Mexico, and Latin America. Additionally, regions may have vocabulary
+that doesn't exist in the other regions where the language is spoken.
+
+<p>So, if I were creating a lexical model just for Latin American Spanish, I
+would need to include the <a href="https://en.wikipedia.org/wiki/UN_M49">UN
+  M49</a> region code. In the case of Latin America, this is <code>419</code>.
+This lexical model would not words that are normal in Spain, but vulgar in
+Latin America, but it would predict words like <span lang="es">pupupsas</span> and
+<span lang="es">chuchitos</span>, which are uncommon words in both Spain and
+Mexico.</p>
+
+<hr>
 
 <p> Once you are finished adding the primary language, click <span
 class="guibutton">OK</span> to return to the <strong>New Lexical Model Project dialog</strong>.
@@ -150,8 +174,11 @@ the primary language, and my model name.</p>
 <p>These identifiers will be combined to make the <code class="language-none">Model ID</code> = <code>Author ID</code>.
   <code class="language-none">Primary Language</code>.<code class="language-none">Unique Name</code></p>
 
-<hr>
-<a id="finished"></a>
+<h2 id="finished">Double-check the information</h2>
+
+<p> Verify that all of the information is correct. Once all of the required
+information has been filled in and verified, click <span class="gui">OK</span>
+to create the project.</p>
 
 <p> Once we have created the project, we can begin to prepare the data! </p>
 
