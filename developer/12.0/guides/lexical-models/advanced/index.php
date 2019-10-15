78b28891e33b4d8b5cb3bcba974c37f30059d92e
Eddie Antonio Santos <easantos@ualberta.ca>
2019-10-15 14:51:48 -0600

    Separate search term to key function documentation.

--- developer/12.0/guides/lexical-models/advanced/index.php
+++ developer/12.0/guides/lexical-models/advanced/index.php
@@ -24,7 +24,7 @@ known as the
 customizations to an existing wordlist project: </p>
 <dl>
   <dt><a href="punctuation.php">Changing the default punctuation</a></dt>
-  <dt><a href="key-function.php">Creating a custom key function</a></dt>
+  <dt><a href="search-term-to-key.php">Creating a custom search key function</a></dt>
   <dt><a href="word-breaker.php">Creating a custom word breaker</a></dt>
 </dl>
 
--- developer/12.0/guides/lexical-models/advanced/model-definition-file.php
+++ developer/12.0/guides/lexical-models/advanced/model-definition-file.php
@@ -111,58 +111,6 @@ text to determine where the words are. </p>
   <p>Main page <a href="search-term-to-key.php"> Search term to key </a></p>
 </aside>
 
-<p> To look up words quickly, the <code>trie</code> model creates a <dfn>
-search key </dfn> that takes the latest word (as determined by the <a
-href="#toc-word-breaking">word breaker</a> and converts it into a “regular” form.
-The purpose of this “regular” form is to make searching for a word work,
-regardless of things such as <strong>accents</strong>,
-<strong>diacritics</strong>, <strong>lettercase</strong>, and minor
-<strong>spelling variations</strong>.
-The ”regular” form is called the <dfn>key</dfn>.  Typically, the key is always
-in lowercase, and lacks all accents and diacritics. For example, the key form
-of “naïve" is "naive" and the keyform of Canada is “canada”. </p>
-
-<p> The form of the word that is stored is “regularized” through the use of a
-<dfn>key function</dfn>, which you can define in TypeScript code. </p>
-
-<p> The key function takes a string, the raw search term, and returns a
-string, being the “regular” key. As an example, consider the <strong>default
-key function</strong>; that is, the key function that is used if you do not
-specify one: </p>
-
-<pre><code class="lang-typescript">searchTermToKey: function (term) {
-  // Use this pattern to remove common diacritical marks.
-  // See: https://www.compart.com/en/unicode/block/U+0300
-  const COMBINING_DIACRITICAL_MARKS = /[\u0300-\u036f]/g;
-
-  // Converts to Unicode Normalization form D.
-  // This means that MOST accents and diacritics have been "decomposed" and
-  // are stored as separate characters. We can then remove these separate
-  // characters!
-  //
-  // e.g., Å → A + ˚
-  let normalizedTerm = term.normalize('NFD');
-
-  // Now, make it lowercase.
-  //
-  // e.g.,  A + ˚ → a + ˚
-  let lowercasedTerm = normalizedTerm.toLowerCase();
-
-  // Now, using the pattern above replace each accent and diacritic with the
-  // empty string. This effectively removes all accents and diacritics!
-  //
-  // e.g.,  a + ˚ → a
-  let termWithoutDiacritics = lowercasedTerm.replace(COMBINING_DIACRITICAL_MARKS, '')
-
-  // The resultant key is lowercased, and has no accents or diacritics.
-  return termWithoutDiacritics;
-},</code></pre>
-
-<p> This should be sufficient for most Latin-based writing systems. However,
-there are cases, such as with SENĆOŦEN, where some characters do not decompose
-into a base letter and a diacritic. In this case, it is necessary to write
-your own key function. </p>
-
 <h2> Write your own custom model </h2>
 
 <p> If all of the above seems too limiting, you can write your own custom
