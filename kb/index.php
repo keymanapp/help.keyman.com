8030a740ef6beacac3ac1cd4d779bbefff2c11b8
Marc Durdin <marc@keyman.com>
2019-11-08 06:44:17 +1100

    chore: add products to articles and tweaks

--- kb/index.php
+++ kb/index.php
@@ -6,6 +6,16 @@
   $id = null;
   $title = 'Knowledge Base index';
 
+  function filename_from_id($id) {
+    $id = intval($id, 10);
+    return sprintf("kb%04.4d.md", $id);
+  }
+
+  function link_from_id($id) {
+    $id = intval($id, 10);
+    return "/kb/$id";
+  }
+
   if(isset($_REQUEST['id'])) {
     $id = $_REQUEST['id'];
     if(!preg_match('/^\d{1,8}$/', $id)) {
@@ -14,21 +24,20 @@
   }
 
   if($id) {
-    $id = intval($id, 10);
-    $filename = sprintf("kb%04.4d.md", $id);
+    $filename = filename_from_id($id);
     $kb = @file_get_contents($filename);
     if($kb === FALSE) {
-      $id = null;
-    }
+      $id = null; // just display index?
+    } else {
+      // We test the first line of the file for a title.
 
-    // We test the first line of the file for a title.
-
-    $text = explode('\n', $kb);
-    if(count($text) > 0) {
-      if(substr($text[0], 0, 2) == '# ') {
-        $title = trim(substr($text[0], 2));
-      } else {
-        $title = $filename;
+      $text = explode('\n', $kb);
+      if(count($text) > 0) {
+        if(substr($text[0], 0, 2) == '# ') {
+          $title = trim(substr($text[0], 2));
+        } else {
+          $title = $filename;
+        }
       }
     }
   }
@@ -42,6 +51,14 @@
   ]);
 
   if($id) {
+    echo "<p>";
+    $pid = intval($id,10) - 1;
+    if($pid > 0) echo "<a href='".link_from_id($pid)."'>&lt; Previous article</a> &nbsp; ";
+    $nid = intval($id,10) + 1;
+    if(file_exists(filename_from_id($nid))) {
+      echo "<a href='".link_from_id($nid)."'>Next article &gt;</a> ";
+    }
+    echo "</p><hr>";
     $Parsedown = new Parsedown();
     echo $Parsedown->text($kb);
   } else {
@@ -50,12 +67,12 @@
     $kbs = glob("kb*.md");
     foreach($kbs as $kb) {
       if(preg_match("/^kb(\d+)\.md$/", $kb, $matches)) {
-        $id = intval($matches[1]);
+        $id = $matches[1];
         $handle = fopen($kb, "r");
         $title = fgets($handle);
         $title = substr($title, 2);
         fclose($handle);
-        echo "<li><a href='/kb/$id'>" . htmlspecialchars($title) . "</a></li>";
+        echo "<li><a href='".link_from_id($id)."'>KMKB{$matches[1]}: " . htmlspecialchars($title) . "</a></li>";
       }
     }
 
