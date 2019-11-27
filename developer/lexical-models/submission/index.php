f1b47b65bf03074ffdc505b39acf02fbae35fb4c
Darcy Wong <darcy_wong@sil.org>
2019-11-27 16:05:31 +0700

    feat: Start a guide on submitting to lexical-models repo

---
+++ developer/lexical-models/submission/index.php
@@ -0,0 +1,28 @@
+<?php
+  require_once('includes/template.php');
+
+  // Required
+  head([
+    'title' =>'Guide: Lexical Model Submissions'
+  ]);
+?>
+
+<h1>Guide for Lexical Model Submissions</h1>
+<p>
+  This is a guide for submitting lexical models to the lexical-models repository.
+  Users will need to be proficient enough with Git to keep their own fork in sync with the upstream branch.
+</p>
+
+<p class="hint">
+  <b>Note:</b> This guide is written for Windows users, so other OS users need to adjust paths accordingly.
+</p>
+
+<dl>
+  <dt><a href="step-0.php">Preliminary Step: Is Your Lexical Model Ready?</a></dt>
+  <dt><a href="step-1.php">Step 1: Setup - One Time Only</a></dt>
+  <dt><a href="step-2.php">Step 2: Before Starting Work on a Lexical Model</a></dt>
+  <dt><a href="step-3.php">Step 3: Development Tools and Building the Lexical Model</a></dt>
+  <dt><a href="step-4.php">Step 4: Organizing the Lexical Model Files</a></dt>
+  <dt><a href="step-5.php">Step 5: Submitting a GitHub Pull Request</a></dt>
+</dl>
+
