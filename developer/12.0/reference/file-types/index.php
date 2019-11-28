486a86a43496951d50eb50a3a16be964335d468a
Darcy Wong <darcy_wong@sil.org>
2019-11-28 11:51:19 +0700

    Add file types and fix constants documentation

--- developer/12.0/reference/file-types/index.php
+++ developer/12.0/reference/file-types/index.php
@@ -10,38 +10,38 @@
 
 <dl>
   <dt><a href="ico">ICO files</a></dt>
-  <dd>Keyboard icon files</dd>
+  <dd>Keyboard icon files (binary file format)</dd>
 
   <dt><a href="kmn">KMN files</a></dt>
-  <dd>Keyboard source files</dd>
+  <dd>Keyboard source files (text file format)</dd>
   
   <dt><a href="kmp">KMP files</a></dt>
-  <dd>Keyboard package files</dd>
+  <dd>Keyboard package files (zip file format)</dd>
   
   <dt><a href="kmx">KMX files</a></dt>
-  <dd>Keyboard files</dd>
+  <dd>Keyboard files (binary file format)</dd>
   
   <dt><a href="kpj">KPJ files</a></dt>
-  <dd>Keyman Developer Project files</dd>
+  <dd>Keyman Developer Project files (XML file format)</dd>
   
   <dt><a href="kps">KPS files</a></dt>
-  <dd>Keyboard package source files</dd>
+  <dd>Keyboard package source files (XML file format)</dd>
   
   <dt><a href="kvk">KVK files</a></dt>
-  <dd>Visual keyboard files</dd>
+  <dd>Visual keyboard files (binary file format)</dd>
 
   <dt><a href="kvks">KVKS files</a></dt>
-  <dd>Visual keyboard source files</dd>
+  <dd>Visual keyboard source files (XML file format)</dd>
 
   <dt><a href="model-js">MODEL.JS files</a></dt>
-  <dd>Lexical model files</dd>
+  <dd>Lexical model files (text file format)</dd>
 
   <dt><a href="model-ts">MODEL.TS files</a></dt>
-  <dd>Lexical model definition files</dd>
+  <dd>Lexical model definition files (text file format)</dd>
 
   <dt><a href="tsv">TSV files</a></dt>
-  <dd>Word list source files, exported in a compatible tab-separated values format.</dd>
+  <dd>Word list source files (tab-separated values file format)</dd>
 
   <dt><a href="keyman-touch-layout">keyman-touch-layout files</a></dt>
-  <dd>Keyman touch layout source files</dd>
+  <dd>Keyman touch layout source files (JSON file format)</dd>
 </dl>
