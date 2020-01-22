7eb43310f41dd8333d9550794e52fbaf294eee23
Marc Durdin <marc@durdin.net>
2020-01-23 07:45:19 +1100

    feat: edit page link
    Provides a quick link to the GitHub editor
    for the page in question. Hope that this
    streamlines quick edits to help pages and
    encourages minor fixes by us and others.

--- _includes/includes/footer.php
+++ _includes/includes/footer.php
@@ -45,8 +45,11 @@
         </div>
     </div>
 </div>
-<div id="jira-feedback">
-  <div id="jira-feedback-tab"><h4><a href='https://community.software.sil.org/c/keyman'>Support</a></h4></div>
+<div class="footer-tab-holder" id='footer-tab-support'>
+  <div class="footer-tab"><h4><a href='https://community.software.sil.org/c/keyman'>Support</a></h4></div>
+</div>
+<div class="footer-tab-holder" id='footer-tab-edit'>
+  <div class="footer-tab"><h4><a href='https://github.com/keymanapp/help.keyman.com/edit/master<?php echo $_SERVER['PHP_SELF'] ?>' target='_blank'>Edit page</a></h4></div>
 </div>
 <div id="KeymanWebControl"></div>
 <script src='<?= cdn('js/prism.js')?>'></script>
