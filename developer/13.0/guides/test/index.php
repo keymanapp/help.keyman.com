bc30721ada86b95b0fbc7b1b717dee19faadcbf8
Marc Durdin <marc@durdin.net>
2020-02-11 20:29:07 +1100

    docs(developer): add index link for testing lexical models

--- developer/13.0/guides/test/index.php
+++ developer/13.0/guides/test/index.php
@@ -13,3 +13,9 @@
   <li><a href="how-to-test-your-keyboard-layout-with-keyman-developer-10-touch-and-desktop">How to test your keyboard layout — touch and desktop</a></li>
   <li><a href="how-to-test-your-touch-layout-in-the-google-chrome-mobile-emulator">How to test your touch layout in the Google Chrome mobile emulator</a></li>
 </ul>
+
+<h1>Testing lexical models</h1>
+
+<ul>
+  <li><a href="how-to-test-your-lexical-model">How to test your lexical model</a></li>
+</ul>
