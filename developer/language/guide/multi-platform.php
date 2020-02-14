f3bd26cce6287338927af6eb2a556b8d2d4e21bf
Marc Durdin <marc@durdin.net>
2020-02-15 07:23:44 +1100

    Update multi-platform.php
    Rewriting some of this page to try and make it more focused on the keyboard designer's point of view . This doesn't cover all the stuff we need -- it still needs more content on philosophy of cross-platform keyboard design.
    
    The discussion also still hinges probably a little too much on Keyman Desktop vs KeymanWeb rather than a holistic all-platform framework view.
    
    So, less on internal implementation, except where it is directly relevant to the keyboard designer.

--- developer/language/guide/multi-platform.php
+++ developer/language/guide/multi-platform.php
@@ -13,6 +13,9 @@ head([
 As it is sometimes beneficial to provide custom-tailored support for the features of particular platforms, please keep the
 following in mind when designing a keyboard to enhance its potential use.</p>
 
+<p>Keyman keyboards run on Windows, macOS, Linux, iOS, and Android devices and in web browsers. There are differences in how keyboards
+  work for each of these platforms and Keyman automatically adapts to each platform as far as possible.</p>
+
 <h3>Keyboard Design</h3>
 
 <p>Keyboards are not a "one size fits all" problem in the world of computing.  Keyboards that work well for desktops and laptops
@@ -24,28 +27,27 @@ layouts.  While not strictly necessary, a keyboard developer should develop a to
     Creating a touch keyboard layout for Amharic</a> guide for an in-depth discussion of the benefits of touch layouts
 and some philosophy on touch keyboard design.</p>
 
+<p>While the presented layout will differ according to the physical device it is running on, the keyboard logic implemented in
+  keyboard rules will be available to all platforms. This includes functionality such as reordering of input, normalization,
+  and prevention of invalid sequences.</p>
+
 <p>In the case that a keyboard should have special features or handling on a given platform, consider use of the
   <a href="/developer/language/reference/platform"><code class="language-keyman">platform()</code></a> statement to allow your keyboard
 the flexibility necessary to accomplish those goals.</p>
 
-<h2>KeymanWeb</h2>
+<h2>Implementation Note</h2>
 
-<p>Of particular note is KeymanWeb, our web-based keyboarding solution designed to work in browsers on any platform or device, touch-enabled or not.
-  As KeymanWeb is also embedded within the iOS and Android apps as their keyboarding engine due to its flexibility, ensuring full compatibility
-  with KeymanWeb will go a long way to making your keyboards fully cross-platform compatible.  Most standard Keyman
-  functionality is available, though there are certain additional restrictions due to historical web development
-  conditions.</p>
+<p>Internally, Keyman for iPhone and iPad and Keyman for Android use KeymanWeb to display the touch keyboard, and to process
+  rules. KeymanWeb keyboards (.js format) support basically all the same functionality as Keyman Desktop keyboards (.kmx format).
+  There are a few statements specific to KeymanWeb that are detailed below.</p>
 
-<p>Of particular note is that KeymanWeb customizes itself quite heavily based on the end-user's browsing device.  When
-  viewed on a standard desktop or laptop computer, it emulates the behavior of Keyman Desktop and Keyman for Mac as
-  closely as possible, displaying a similar OSK when requested.  On mobile devices, such as tablets or phones, KeymanWeb can
-  adjust and use an optimized touch-layout keyboard if one is provided by the keyboard designer.  If one is not provided, the
-  basic 'desktop'-style keyboard can also work on mobile devices, though it is generally suboptimal because keys are organised
-  as in a physical layout, resulting in many keys on each layer.</p>
+<p>If you do not provide a touch layout in your keyboard design, KeymanWeb will use the desktop On Screen Keyboard (.kvk / .kvks format)
+  data. While the basic 'desktop'-style keyboard can work on mobile devices, it is generally suboptimal because keys are organised
+  as in a physical layout, resulting in too many keys on each layer, which are small and hard to select reliably.</p>
 
-<h3>Additional Stores</h3>
+<h3>Additional Stores for KeymanWeb</h3>
 
-<p>The following lists additional information that you can pass to the compiler.</p>
+<p>The following Keyman Keyboard Language system stores provide functionality specific to KeymanWeb:</p>
 
 <ul>
   <li><a href="/developer/language/reference/kmw_embedcss" title="&amp;kmw_embedcss store"><code class="language-keyman">&amp;kmw_embedcss</code>:</a>
@@ -58,25 +60,26 @@ the flexibility necessary to accomplish those goals.</p>
   Specifies the embedded help text for web keyboards</li>
   <li><a href="/developer/language/reference/kmw_rtl" title="&amp;kmw_rtl store"><code class="language-keyman">&amp;kmw_rtl</code>:</a>
   Specifies the directionality of text controls for web keyboards</li>
-  <li><a href="/developer/language/reference/visualkeyboard"><code class="language-keyman">&amp;VisualKeyboard</code>:</a>
-  Use a store with this name to reference the .kvk file to embed into the KeymanWeb keyboard.</li>
 </ul>
 
 <h3>Recommendations</h3>
 
 <ul>
-<li>Only Unicode keyboards are supported.</li>
-<li>Avoid using virtual keys where possible, as some platforms do not support all virtual keys.  In particular, Opera does
-not support ALT.</li>
-<li>KeymanWeb presently cannot distinguish between left and right Ctrl/Alt, as most of the major browsers used to lack
-  support for this.  You may wish to consider use of the <a href="/developer/language/reference/platform">
-    <code class="language-keyman">platform()</code> statement</a> to provide workarounds for this in the web environment.</li>
-<li>Do include a visual keyboard and help information - this makes the keyboard vastly more useable.</li>
+  <li>Only Unicode keyboards are supported for KeymanWeb (.js format) keyboards.</li>
+  <li>When using keys with modifiers, use just Shift, AltGr (Right Alt) and Shift+AltGr. The left-hand modifiers should be reserved
+    for system functions (such as hotkeys), and the Right Ctrl modifier can also be difficult, especially on macOS.</li>
+  <li>When designing your touch layout, layers correspond roughly to modifiers, but avoid following the modifier paradigm for any
+    layer apart from Shift: it usually makes more sense to use long-press menus rather than having an AltGr layer.</li>
+  <li>Do include a visual keyboard and help information - this makes the keyboard vastly more useable.</li>
   <li>Save your keyboard file with a filename comprised of the letters a-z (lower case), digits 0-9, and underscore (_).
-    <ul><li>See <a href="/developer/keyboards/#toc-use-ascii-characters-for-keyboard-base-names">our keyboard submission guidelines page</a>
-    for more details.</li></li></ul>
-<li>Virtual key output is not supported.</li>
-<li>The <code class="language-keyman">platform()</code> statement and <a href='compile-targets'><code class="language-keyman">$keymanonly</code>
+    <ul>
+      <li>See <a href="/developer/keyboards/#toc-use-ascii-characters-for-keyboard-base-names">our keyboard submission guidelines page</a>
+    for more details.</li></ul>
+  </li>
+  <li>Virtual key output is not supported: this is legacy functionality that has many compatibility concerns and
+    should not be used on any platform.</li>
+  <li>The <a href='/developer/language/reference/platform'><code class="language-keyman">platform()</code> statement</a> and 
+    <a href='compile-targets'><code class="language-keyman">$keymanonly</code>
     and <code class="language-keyman">$keymanweb</code> compile targets</a> can be used to manage implementation differences
-  between the two platforms. Where possible, use <code>platform()</code> statements.
+    between the two platforms. Where possible, use <code>platform()</code> statements.</li>
 </ul>
