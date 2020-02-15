cb4d74475e660b9fe9e38c1712ab4bee2394a832
Darcy Wong <darcy_wong@sil.org>
2020-02-15 17:14:39 +0700

    Copy developer/engine/android/12.0 to 13.0

---
+++ developer/engine/android/13.0/KMManager/addKeyboard.php
@@ -0,0 +1,51 @@
+<?php
+    require_once('includes/template.php');
+    $class = 'KMManager';
+    $method = 'addKeyboard';
+    $param1 = 'context';
+    $param2 = 'keyboardInfo';
+    $methodSyntax = sprintf('<var>%s</var>.%s(<var>Context %s</var>, <var>HashMap&lt;String, String&gt; %s</var>)', $class, $method, $param1, $param2);
+    head(['title' => $class.'.'.$method.'()']);
+?>
+
+<h1>KMManager.addKeyboard()</h1>
+
+<h2 id="Summary" name="Summary">Summary</h2>
+<p>The <code><strong><?php echo $method.'()' ?></strong></code> method adds a keyboard into the keyboards list.</p>
+
+<h2 id="Syntax" name="Syntax">Syntax</h2>
+<pre class="language-javascript"><code><?php echo $methodSyntax ?></code></pre>
+
+<h3 id="Parameters" name="Parameters">Parameters</h3>
+<dl>
+ <dt><code><?php echo $param1 ?></code></dt>
+ <dd>The context.</dd>
+<dt><code><?php echo $param2 ?></code></dt>
+<dd>A dictionary of keyboard information with keys and values defined as <code>HashMap&lt;String key, String value&gt;</code>.</dd>
+</dl>
+
+<h3 id="Returns" name="Returns">Returns</h3>
+<p>Returns <code>true</code> if the keyboard was added successfully, <code>false</code> otherwise.</p>
+
+<h2 id="Description" name="Description">Description</h2>
+<p>Use this method to include a keyboard in the keyboards list so that it can be selected from the keyboards menu. If the keyboard with same keyboard ID and language ID exists, it updates the existing keyboard info.</p>
+
+<h2 id="Examples">Examples</h2>
+
+<h3 id="Example:_Using_method" name="Example:_Using_method">Example: Using <code><?php echo $method.'()' ?></code></h3>
+<p>The following script illustrate the use of <code><?php echo $method.'()' ?></code>:</p>
+<pre class="language-javascript line-numbers"><code>    // Add a custom keyboard
+    HashMap&lt;String, String&gt; kbInfo = new HashMap&lt;String, String&gt;();
+    kbInfo.put(KMManager.KMKey_KeyboardID, "tamil99m");
+    kbInfo.put(KMManager.KMKey_LanguageID, "ta");
+    kbInfo.put(KMManager.KMKey_KeyboardName, "Tamil 99M");
+    kbInfo.put(KMManager.KMKey_LanguageName, "Tamil");
+    kbInfo.put(KMManager.KMKey_KeyboardVersion, "1.1");
+    kbInfo.put(KMManager.KMKey_Font, "aava1.ttf");
+    KMManager.addKeyboard(this, kbInfo);
+</code></pre>
+
+<h2 id="See_also" name="See_also">See also</h2>
+<ul>
+ <li><a href="removeKeyboard.php"><code>removeKeyboard()</code></a></li>
+</ul>
---
+++ developer/engine/android/13.0/index.php
@@ -0,0 +1,44 @@
+<?php
+  require_once('includes/template.php');
+  
+  head([
+    'title' => 'Keyman Engine for Android'
+  ]);
+?>
+<h1 class="center">Keyman Engine for Android</h1>
+
+<h2 id="Overview" name="Overview">Overview</h2>
+
+<p>Keyman Engine for Android 12.0 is a Java library for Android 4.1 and later versions which enables a fully customisable keyboard layout, both within an app and system-wide.
+Keyboard layouts for Keyman Engine can be created with <a href='/developer/12.0'>Keyman Developer</a>, and a <a href='http://keyman.com/developer/keymanweb/keyboards.php'>library of existing keyboard layouts</a>
+is also available.</p>
+
+<h2 id="Guides" name="Guides"><a href='guides/'>Guides</a></h2>
+
+<ul>
+  <li><a href='guides/in-app/'>Build an in-app keyboard</a></li>
+  <li><a href='guides/system-keyboard/'>Build a system keyboard app</a></li>
+</ul>
+
+<h2 id="Classes" name="Classes">Classes</h2>
+
+<dl>
+  <dt><code><a href='KMManager/'>KMManager</a></code></dt>
+  <dd>The core class for controlling Keyman Engine</dd>
+
+  <dt><code><a href='KeyboardEventHandler/'>KeyboardEventHandler</a></code></dt>
+  <dd>Provides keyboard events and methods to notify registered listeners</dd>
+</dl>
+
+<h2 id="See_also" name="See_also">See also</h2>
+
+<ul>
+  <li><a href="/developer/engine/android/12.0/" title="Keyman Engine for Android 12.0">Keyman Engine for Android 12.0</a></li>
+  <li><a href="/developer/engine/android/11.0/" title="Keyman Engine for Android 11.0">Keyman Engine for Android 11.0</a></li>
+  <li><a href="/developer/engine/android/10.0/" title="Keyman Engine for Android 10.0">Keyman Engine for Android 10.0</a></li>
+  <li><a href="/developer/engine/android/2.0/" title="Keyman Engine for Android 2.0">Keyman Engine for Android 2.0</a></li>
+  <li><a href="/developer/12.0/" title="Keyman Developer 12.0">Keyman Developer 12.0</a></li>
+  <li><a href='http://keyman.com/developer/keymanweb/keyboards.php' title="Keyboard Library">Keyboard Library</a></li>
+  <li><a href="/developer/engine/iphone-and-ipad/12.0/" title="Keyman Engine for iPhone and iPad 12.0">
+    Keyman Engine for iPhone and iPad 12.0</a></li>
+</ul>
