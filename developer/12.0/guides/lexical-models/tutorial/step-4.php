925fe9313b7516e2090c84cf8458cb3afc384191
Eddie Antonio Santos <easantos@ualberta.ca>
2020-02-26 13:15:37 -0700

    docs(common/lmlayer): conclude tutorial with next steps

--- developer/12.0/guides/lexical-models/tutorial/step-4.php
+++ developer/12.0/guides/lexical-models/tutorial/step-4.php
@@ -10,7 +10,7 @@ head([
 <h1>Step 4: Compile the lexical model</h1>
 
 <p>
-  Before we can test the lexical model, we must compile it. In this step, the <code>.tsv</code> wordlist and
+  Before use the lexical model, we must compile it. In this step, the <code>.tsv</code> wordlist and
   <a href="../advanced/model-definition-file.php">the model definition file</a> get <dfn>compiled</dfn>
   into a single <code>.model.js</code> file. This file is the one that Keyman
   uses internally to generate suggestions.
@@ -22,10 +22,6 @@ head([
 <p>In Keyman Developer project view, select the "Models" tab.</p>
 
 <figure>
-<?php /*
-  <img> within <figure> with a <figcaption> do not require alt="" attribute
-  See: https://www.w3.org/TR/2014/REC-html5-20141028/embedded-content-0.html#figcaption-as-alt-condition
-*/ ?>
   <img src="<?= cdn('img/developer/120/ui/frmModelsCompile.png') ?>" alt="The “Project - Models” tab in Keyman Developer" />
 </figure>
 
@@ -33,4 +29,20 @@ head([
   The Message window will display the results of the compilation. If you have no typing errors, the lexical model should compile successfully.
   If successful, this will create a <code>.model.js</code> file in a build subdirectory of the lexical model project directory.</p>
 
-<p>Now that the model is built, we are ready to test our lexical model.</p>
+
+<h2> Next steps </h2>
+
+<p> This concludes the lexical model tutorial. Here's what's next: </p>
+
+<ul>
+  <li>
+    Now that the model is built, we are ready to
+    <a href="../distribute/packages.php">distribute our lexical model</a>.
+  </li>
+  <li> (<em>For advanced users</em>)
+    We can do some
+    <a href="../advanced/">advanced customization</a> by modifying the
+    <a href="../advanced/model-definition-file.php">model definition file</a>.
+    Advanced customizations require a some comfort with modifying code!
+  </li>
+</ul>
