d0a6a4ec1fb3f7afbe2be7d720d650521516e018
Darcy Wong <darcywong00@users.noreply.github.com>
2020-02-27 21:20:35 +0700

    Update developer/13.0/reference/file-types/ttf.php
    Co-Authored-By: Marc Durdin <marc@durdin.net>

--- developer/13.0/reference/file-types/ttf.php
+++ developer/13.0/reference/file-types/ttf.php
@@ -14,7 +14,9 @@
 
   <dt>Description:</dt>
   <dd>A .ttf file is a TrueType font file. This file is distributed with a keyboard package
-    to be used as a keyboard font or display font.</dd>
+    to be used as a keyboard font or display font. This file is optional; many languages and
+    keyboards do not need to include a font because there will be a font supplied by the
+    operating system.</dd>
 
   <dt>Details:</dt>
   <dd>A .ttf file is a binary file. It is included using the Package Editor in Keyman Developer.
