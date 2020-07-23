dded4ff0fe2fb75b3d49208e1e9b7ca4c47aa5eb
Marc Durdin <marc@durdin.net>
2020-07-23 10:28:12 +1000

    chore: add KeymanHosts.php to _common

--- _includes/includes/template.php
+++ _includes/includes/template.php
@@ -1,10 +1,14 @@
 <?php
-  require __DIR__ . '/../../vendor/autoload.php';
-  Sentry\init(['dsn' => 'https://fcc8fe39792f49f3a94ea831cad5c9d6@sentry.keyman.com/2' ]);
+  require_once __DIR__ . '/../../vendor/autoload.php';
+  require_once __DIR__ . '/../../_common/KeymanSentry.php';
+
+  const SENTRY_DSN = 'https://fcc8fe39792f49f3a94ea831cad5c9d6@sentry.keyman.com/2';
+  \Keyman\Site\Common\KeymanSentry::init(SENTRY_DSN);
 
   require_once('includes/servervars.php');
   require_once('index-content.php');
   require_once('page-version.php');
+
 
   function template_finish($foot, $embed_template) {
     //ob_end_flush();
