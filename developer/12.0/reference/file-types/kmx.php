79734482ea4d5a7862d0f40a8c0c67a55fe5e01d
Darcy Wong <darcy_wong@sil.org>
2020-09-16 10:25:17 +0700

    fix: Update kmx usage
    Fixes #197
    
    Starting with 11.0, Keyman for Linux also uses kmx.

--- developer/12.0/reference/file-types/kmx.php
+++ developer/12.0/reference/file-types/kmx.php
@@ -10,15 +10,16 @@
 
 <dl>
   <dt>Used by:</dt>
-  <dd><span class="application">Keyman Desktop</span>.</dd>
+  <dd><span class="application">Keyman Desktop</span>, <span class="application">Keyman for macOS</span>,
+    and <span class="application">Keyman for Linux</span>.</dd>
 
   <dt>Description:</dt>
   <dd>A .KMX file is a compiled keyboard file.</dd>
 
   <dt>Details:</dt>
   <dd>A .KMX file is compiled in <span class="application">Keyman Developer</span> from the Keyman source file
-    (<a href='kmn'>.KMN</a>) and icon for the keyboard (.ICO/.BMP). Note that .KMX files will only work in 
-    <span class="application">Keyman Desktop</span>.</dd>
+    (<a href='kmn'>.KMN</a>) and icon for the keyboard (.ICO/.BMP). Note that .KMX files are not used for
+    <span class="application">Keyman for Android</span> and <span class="application">Keyman for iPhone and iPad</span>.</dd>
 
   <dt>Distributed with keyboard:</dt>
   <dd>This is the keyboard file. It must be distributed with your package to allow people to type using the
