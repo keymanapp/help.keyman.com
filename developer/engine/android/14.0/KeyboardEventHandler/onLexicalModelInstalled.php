76574ecc55d10d8e9b0f989f6c4000b7d16271ca
Darcy Wong <darcy_wong@sil.org>
2020-09-25 08:15:26 +0700

    fix: Update KeyboardEventHandler
    Split out the two interfaces, and add `onPackageInstalled` and `onLexicalModelInstalled`

---
+++ developer/engine/android/14.0/KeyboardEventHandler/onLexicalModelInstalled.php
@@ -0,0 +1,40 @@
+<?php
+    require_once('includes/template.php');
+    $class = '';
+    $event = 'onLexicalModelInstalled';
+    $param1 = 'lexicalModelsInstalled';
+    $eventSyntax = sprintf('public void %s(List&lt;<var>HashMap&lt;String key, String value&gt;&gt; %s</var>)', $event, $param1);
+    head(['title' => $event.'()']);
+?>
+
+<h1>onLexicalModelInstalled()</h1>
+
+<h2 id="Summary" name="Summary">Summary</h2>
+<p>The <code><strong><?php echo $event.'()' ?></strong></code> event is called when a lexical-model package has been installed.</p>
+
+<h2 id="Syntax" name="Syntax">Syntax</h2>
+<pre class="language-javascript"><code><?php echo $eventSyntax ?></code></pre>
+
+<h3 id="Parameters" name="Parameters">Parameters</h3>
+<dl>
+ <dt><code><?php echo $param1 ?></code></dt>
+<dd>The information dictionaries of the lexical-models installed from a lexical-model package with keys and values defined as <code>List&lt;HashMap&lt;String key, String value&gt;&gt;</code>.</dd>
+</dl>
+
+<h2 id="Description" name="Description">Description</h2>
+<p>Implement this method to handle lexical-model package installed event.</p>
+
+<h2 id="Examples">Examples</h2>
+
+<h3 id="Example:_Using_method” name="Example:_Using_method">Example: Using <code><?php echo $event.'()' ?></code></h3>
+<p>The following script illustrate the use of <code><?php echo $event.'()' ?></code>:</p>
+<pre class="language-javascript line-numbers"><code>    @Override
+    public void onLexicalModelInstalled(List<HashMap&lt;String key, String value&gt;> lexicalModelsInstalled) {
+        // handle lexical-model package installed event here
+    }
+</code></pre>
+
+<h2 id="See_also" name="See_also">See also</h2>
+<ul>
+ <li><a href="onPackageInstalled.php" title="Title."><code>onPackageInstalled()</code></a></li>
+</ul>
