050204e0fa935f339f341d21ad7444c69b6f9bd9
Marc Durdin <marc@durdin.net>
2020-10-29 16:14:35 +1100

    chore: reboot deprecation for Keyman Engine for Windows

--- developer/engine/desktop/10.0/api/IKeymanSystemInfo/RebootRequired.php
+++ developer/engine/desktop/10.0/api/IKeymanSystemInfo/RebootRequired.php
@@ -15,6 +15,8 @@ performed through the Keyman Engine API cannot be completed until the system is 
 arise if a file is locked by another process at install time, and is most likely to occur with font installation
 and removal.</p>
 
+<p><b>Note:</b> In Keyman 14.0 and later, <code>RebootRequired</code> always returns <code>False</code>.</p>
+
 <h2>Specification</h2>
 
 <pre class="language-clike"><code>readonly bool RebootRequired</code></pre>
