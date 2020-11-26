d8c3805e4952c5d8a03e87663edb21a88d5dc813
Eberhard Beilharz <eb1@sil.org>
2020-11-26 11:39:48 +0100

    chore(common): add hr before KB menu item

--- _includes/includes/phone-menu.php
+++ _includes/includes/phone-menu.php
@@ -35,6 +35,7 @@
                 <li><a href="/products/iphone-and-ipad/">Keyman for iPhone and iPad</a></li>
                 <li><a href="/products/android/">Keyman for Android</a></li>
                 <li><a href="/products/bookmarklet/">Keyman Bookmarklet</a></li>
+                <hr/>
                 <li><a href="/knowledge-base/">Knowledge Base</a></li>
             </ul>
         </div>
