98fedb28a6463d80c7d06a33f4db50d0dd165f83
Keyman Build Server <keyman-server@users.noreply.github.com>
2020-12-02 17:27:38 -0500

    Keyman for Windows help deployment (automatic)

--- products/desktop/14.0/docs/basic_toolbox.php
+++ products/desktop/14.0/docs/basic_toolbox.php
@@ -2,12 +2,12 @@
   require_once('includes/template.php');
 
   head([
-    'title' => "The Keyman Desktop Toolbox"
+    'title' => "The Keyman Toolbox"
   ]);
 ?>
-<div class="navheader"><table width="100%" summary="Navigation header"><tr><th colspan="3" align="center">The Keyman Desktop Toolbox</th></tr><tr><td width="20%" align="left"><a accesskey="p" href="basic_traymenu.php">Prev</a> </td><th width="60%" align="center">Chapter 3. Basic Help</th><td width="20%" align="right"> <a accesskey="n" href="basic_osk.php">Next</a></td></tr></table><hr></div><div class="section" lang="en"><div class="titlepage"><div><div><h2 class="title" style="clear: both" id="basic_toolbox">The Keyman Desktop Toolbox</h2></div></div></div><p>The Keyman Toolbox contains a collection of tools for working with Keyman Desktop.</p><p>
+<div class="navheader"><table width="100%" summary="Navigation header"><tr><th colspan="3" align="center">The Keyman Toolbox</th></tr><tr><td width="20%" align="left"><a accesskey="p" href="basic_traymenu.php">Prev</a> </td><th width="60%" align="center">Chapter 3. Basic Help</th><td width="20%" align="right"> <a accesskey="n" href="basic_osk.php">Next</a></td></tr></table><hr></div><div class="section" lang="en"><div class="titlepage"><div><div><h2 class="title" style="clear: both" id="basic_toolbox">The Keyman Toolbox</h2></div></div></div><p>The Keyman Toolbox contains a collection of tools for working with Keyman.</p><p>
     <span class="inlinemediaobject"><img src="desktop_images/osk_keyboard.png"></span>
-  </p><h3><a name="id595686"></a>Opening the Keyman Toolbox</h3><p>To display the Keyman Toolbox:</p><div class="orderedlist"><ol type="1"><li><p>Click on the <span class="guiicon">Keyman Desktop</span> icon <span class="inlinemediaobject"><img src="desktop_images/icon-keyman.png"></span>, on the Windows Taskbar near the clock.</p></li><li><p>From the Keyman Desktop menu, select <span class="guilabel">On Screen Keyboard</span>.</p></li></ol></div><div class="note" style="margin-left: 0.5in; margin-right: 0.5in;"><h3 class="title">Note</h3>The Keyman Toolbox is displayed by default in the last position it was used. It will be displayed initially in the bottom right hand corner of the screen.</div><div class="note" style="margin-left: 0.5in; margin-right: 0.5in;"><h3 class="title">Note</h3>The Keyman Toolbox opens automatically when you select a Keyman keyboard layout. You can change this behaviour in the <a class="link" href="basic_options_tab.php" title="Keyman Configuration - Options Tab">Options tab</a> of Keyman Configuration.</div><h3><a name="id595740"></a>Using the Keyman Toolbox</h3><p>Along the top of the Keyman Toolbox is a toolbar.</p><p>
+  </p><h3><a name="id572310"></a>Opening the Keyman Toolbox</h3><p>To display the Keyman Toolbox:</p><div class="orderedlist"><ol type="1"><li><p>Click on the <span class="guiicon">Keyman</span> icon <span class="inlinemediaobject"><img src="desktop_images/icon-keyman.png"></span>, on the Windows Taskbar near the clock.</p></li><li><p>From the Keyman menu, select <span class="guilabel">On Screen Keyboard</span>.</p></li></ol></div><div class="note" style="margin-left: 0.5in; margin-right: 0.5in;"><h3 class="title">Note</h3>The Keyman Toolbox is displayed by default in the last position it was used. It will be displayed initially in the bottom right hand corner of the screen.</div><div class="note" style="margin-left: 0.5in; margin-right: 0.5in;"><h3 class="title">Note</h3>The Keyman Toolbox opens automatically when you select a Keyman keyboard layout. You can change this behaviour in the <a class="link" href="basic_options_tab.php" title="Keyman Configuration - Options Tab">Options tab</a> of Keyman Configuration.</div><h3><a name="id572361"></a>Using the Keyman Toolbox</h3><p>Along the top of the Keyman Toolbox is a toolbar.</p><p>
     <span class="inlinemediaobject"><img src="desktop_images/toolbox-toolbar.png"></span>
   </p><p>Any of the following buttons may appear in the toolbar:</p><div class="informaltable"><table border="0"><colgroup><col><col><col></colgroup><tbody><tr><td>
             <p>Icon</p>
@@ -54,7 +54,7 @@
           </td><td>
             <p>Open Keyman Configuration</p>
           </td><td>
-            <p>Opens <a class="link" href="basic_config_menu.php" title="Keyman Desktop Configuration">Keyman Configuration</a>, where you can install or uninstall keyboard layouts and change various Keyman options and settings.</p>
+            <p>Opens <a class="link" href="basic_config_menu.php" title="Keyman Configuration">Keyman Configuration</a>, where you can install or uninstall keyboard layouts and change various Keyman options and settings.</p>
           </td></tr><tr><td>
             <p>
               <span class="inlinemediaobject"><img src="desktop_images/toolboxicon-help.png"></span>
@@ -62,7 +62,7 @@
           </td><td>
             <p>Open Help</p>
           </td><td>
-            <p>Opens <a class="link" href="basic_help.php" title="The Keyman Desktop Help Menu">Keyman Desktop Help</a>.</p>
+            <p>Opens <a class="link" href="basic_help.php" title="The Keyman Help Menu">Keyman Help</a>.</p>
           </td></tr><tr><td>
             <p>
               <span class="inlinemediaobject"><img src="desktop_images/toolboxicon-close.png"></span>
@@ -70,8 +70,8 @@
           </td><td>
             <p>Close Keyman Toolbox</p>
           </td><td>
-            <p>Closes the Keyman Toolbox but does not exit Keyman Desktop.  The Keyman Toolbox can be displayed again by following the instructions at the top of this help topic.</p>
-          </td></tr></tbody></table></div><p>You can move the Keyman Toolbox around your screen by clicking and dragging anywhere on the Keyman Toolbox toolbar not covered by a button.</p><p>You can resize the Keyman Toolbox by clicking and dragging on a corner of the Keyman Toolbox.</p><div class="note" style="margin-left: 0.5in; margin-right: 0.5in;"><h3 class="title">Note</h3>When the On Screen Keyboard tool is selected, the Keyman Toolbox will only resize according to the proportions of the keyboard.</div><h3><a name="id582180"></a>Toolbox Options</h3><p>
+            <p>Closes the Keyman Toolbox but does not exit Keyman.  The Keyman Toolbox can be displayed again by following the instructions at the top of this help topic.</p>
+          </td></tr></tbody></table></div><p>You can move the Keyman Toolbox around your screen by clicking and dragging anywhere on the Keyman Toolbox toolbar not covered by a button.</p><p>You can resize the Keyman Toolbox by clicking and dragging on a corner of the Keyman Toolbox.</p><div class="note" style="margin-left: 0.5in; margin-right: 0.5in;"><h3 class="title">Note</h3>When the On Screen Keyboard tool is selected, the Keyman Toolbox will only resize according to the proportions of the keyboard.</div><h3><a name="id557719"></a>Toolbox Options</h3><p>
     <span class="inlinemediaobject"><img src="desktop_images/toolbox-options.png"></span>
   </p><p>Right click on the Keyman Toolbox to display a menu with the following options:</p><div class="informaltable"><table border="0"><colgroup><col><col></colgroup><tbody><tr><td>
             <p>Fade When Inactive</p>
@@ -89,9 +89,9 @@
             <p>Print...</p>
           </td><td>
             <p>This option prints the keyboard view of the current keyboard layout for easy reference. The printout will include diagrams for every state of the current keyboard layout, including shift states.</p>
-          </td></tr></tbody></table></div><h3><a name="id582278"></a>Toolbox Hints</h3><p>
+          </td></tr></tbody></table></div><h3><a name="id567788"></a>Toolbox Hints</h3><p>
     <span class="inlinemediaobject"><img src="desktop_images/toolbox-hints.png"></span>
-  </p><p>At the bottom of the Keyman Toolbox, a hint bar shows handy tips on how to use Keyman Desktop and access some of the more       advanced features available.  The hint bar will slowly cycle through the available hints, or you can use the arrow buttons on the right of the hint bar to cycle through the hints yourself.</p><p>You can close the hint bar by clicking the X icon on the right hand side of the hint bar.  This X icon is not the same as the Close Keyman Toolbox button at the top of the window!</p><p>To access the hint bar again, open the <a class="link" href="basic_options_tab.php" title="Keyman Configuration - Options Tab">Options tab</a> of Keyman Configuration and click <span class="guibutton">Reset Hints</span>.</p><h3><a name="id582319"></a>Related Topics</h3><div class="itemizedlist"><ul type="disc"><li><p>
+  </p><p>At the bottom of the Keyman Toolbox, a hint bar shows handy tips on how to use Keyman and access some of the more       advanced features available.  The hint bar will slowly cycle through the available hints, or you can use the arrow buttons on the right of the hint bar to cycle through the hints yourself.</p><p>You can close the hint bar by clicking the X icon on the right hand side of the hint bar.  This X icon is not the same as the Close Keyman Toolbox button at the top of the window!</p><p>To access the hint bar again, open the <a class="link" href="basic_options_tab.php" title="Keyman Configuration - Options Tab">Options tab</a> of Keyman Configuration and click <span class="guibutton">Reset Hints</span>.</p><h3><a name="id567829"></a>Related Topics</h3><div class="itemizedlist"><ul type="disc"><li><p>
     <a class="xref" href="basic_osk.php" title="Keyman Toolbox - On Screen Keyboard">Keyman Toolbox - On Screen Keyboard</a>
    </p></li><li><p>
     <a class="xref" href="basic_usage.php" title="Keyman Toolbox - Keyboard Usage">Keyman Toolbox - Keyboard Usage</a>
@@ -99,4 +99,4 @@
     <a class="xref" href="basic_fonthelper.php" title="Keyman Toolbox - Font Helper">Keyman Toolbox - Font Helper</a>
    </p></li><li><p>
     <a class="xref" href="basic_charactermap.php" title="Keyman Toolbox - Character Map">Keyman Toolbox - Character Map</a>
-   </p></li></ul></div></div><div class="navfooter"><hr><table width="100%" summary="Navigation footer"><tr><td width="40%" align="left"><a accesskey="p" href="basic_traymenu.php">Prev</a> </td><td width="20%" align="center"><a accesskey="u" href="basic.php">Up</a></td><td width="40%" align="right"> <a accesskey="n" href="basic_osk.php">Next</a></td></tr><tr><td width="40%" align="left" valign="top">The Keyman Desktop Menu </td><td width="20%" align="center"><a accesskey="h" href="index.php">Home</a></td><td width="40%" align="right" valign="top"> Keyman Toolbox - On Screen Keyboard</td></tr></table></div>
+   </p></li></ul></div></div><div class="navfooter"><hr><table width="100%" summary="Navigation footer"><tr><td width="40%" align="left"><a accesskey="p" href="basic_traymenu.php">Prev</a> </td><td width="20%" align="center"><a accesskey="u" href="basic.php">Up</a></td><td width="40%" align="right"> <a accesskey="n" href="basic_osk.php">Next</a></td></tr><tr><td width="40%" align="left" valign="top">The Keyman Menu </td><td width="20%" align="center"><a accesskey="h" href="index.php">Home</a></td><td width="40%" align="right" valign="top"> Keyman Toolbox - On Screen Keyboard</td></tr></table></div>
