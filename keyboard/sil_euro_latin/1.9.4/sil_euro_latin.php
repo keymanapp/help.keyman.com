0134ceab2c8fe49a648347bb5663913dfac09ad2
Keyman Build Server <keyman-server@users.noreply.github.com>
2021-03-19 20:01:06 -0400

    Keyboard help deployment (automatic)

---
+++ keyboard/sil_euro_latin/1.9.4/sil_euro_latin.php
@@ -0,0 +1,50 @@
+<?php
+  $pagename = 'EuroLatin (SIL) Keyboard Help';
+  $pagetitle = 'EuroLatin (SIL) Keyboard Help';
+  require_once('header.php');
+?>
+<p>The EuroLatin keyboard enables you to type in <b>all</b> European languages which use Latin-script and many Latin-script languages from the rest of the world. However, the keyboard should not be expected to be enhanced for languages outside of Europe. The keyboard is designed to be used with any Latin hardware keyboard. It uses simple sequences of <code>symbol + character</code> to type letters and other characters not available on a standard keyboard.</p>
+
+<h1>Fonts</h1>
+
+<p>Most of the characters in this keyboard are included in the fonts Calibri, Cambria, Arial, and Times New Roman, as well as many other fonts. Some of the characters in this keyboard are only supported by specialty fonts, like Code2000.</p>
+
+<h1>Complete Typing Charts</h1>
+
+<h2>Lowercase Letters</h2>
+<p>Type the symbol on the left and then the letter (from the bottom row) to get the character in the chart.</p>
+<img src="lowercase.png" />
+<h2 class="newpage">Uppercase Letters</h2>
+<p>Type the symbol on the left and then the letter (from the bottom row) to get the character in the chart.</p>
+<img src="uppercase.png" />
+<h2 class="newpage">Digraphs</h2>
+<p>Type the characters in the bottom box to get the character in the top box.</p>
+<img src="digraphs.png" />
+<h2>Punctuation and Symbols</h2>
+<p>Type the characters in the bottom box to get the character in the top box.</p>
+<p>Note that "\-" outputs the code for <i>soft hyphen</i>, code-point (173, U+00AD).</p>
+<img src="symbols.png" />
+<h2>Numbers and Fractions</h2>
+<p>Type the characters in the bottom box to get the character in the top box.</p>
+<img src="numbers.png" />
+<h2>Currency Symbols</h2>
+<p>Type the characters in the bottom box to get the character in the top box.</p>
+<img src="currency.png" />
+<h2>Prefix keys</h2>
+<p>Type the characters in the bottom box to get the character in the top box.</p>
+<img src="prefix.png" />
+
+<div id='osk-phone-container'>
+    <h2>Mobile Keyboard Layout</h2>
+    <div id='osk-phone' data-states='default shift numeric symbol currency'></div>
+</div>
+
+<div style='clear: both'></div>
+
+<h1 class='newpage'>Technical Support</h1>
+<p>You can access this help again from the Keyboard Usage tool on the Keyman Desktop toolbar</p>
+
+<h1>Keyboard Source</h1>
+
+<p>This keyboard is available online at <a href='https://keyman.com/keyboards/sil_euro_latin'>keyman.com/keyboards/sil_euro_latin</a></p>
+
