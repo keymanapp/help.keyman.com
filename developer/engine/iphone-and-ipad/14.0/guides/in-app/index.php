d2cca512730fed8a51e1d381a033e81d20e9f5bb
jahorton <jahorton@sil.org>
2021-03-24 14:46:58 +0700

    chore: full overhaul of iOS KMSample1 guide (in-app)

--- developer/engine/iphone-and-ipad/14.0/guides/in-app/index.php
+++ developer/engine/iphone-and-ipad/14.0/guides/in-app/index.php
@@ -55,83 +55,63 @@
   <li><a href="/developer/current-version/guides/test/keyboard-touch-mobile-emulator">
       How to test your touch layout in the Google Chrome mobile emulator</a></li>
 </ul>
-<p>When your keyboard is ready, you should have a compiled keyboard Javascript file. The example below shows the
-  Tamil Mobile99 touch layout.</p>
+<p>When your keyboard is ready, you should have a compiled keyboard package. The example below shows the
+  Thamizha Tamil99 touch layout.</p>
 <p><a class="asset-img-link" style="display: inline;"
-      href="<?php echo cdn("img/engine/android/10.0/guides/in-app/touch-layout.png"); ?>"><img
+      href="<?php echo cdn("img/engine/ios/14.0/guides/in-app/keyboard-layout-in-developer.png"); ?>"><img
         class="asset asset-image at-xid-6a00d83432368053ef01b7c759d462970b image-full img-responsive"
         title="Keyman-developer"
-        src="<?php echo cdn("img/engine/android/10.0/guides/in-app/touch-layout-800wi.png"); ?>"
+        src="<?php echo cdn("img/engine/ios/14.0/guides/in-app/keyboard-layout-in-developer-800wi.png"); ?>"
         alt="Keyman-developer" border="0"/></a></p>
-<p>Open the containing folder for the keyboard to find the Javascript file to copy over; its name will be based on
-  your source keyboard name.</p>
+<p>Open the package tab for your keyboard and compile it.  Then, open the folder containing your package</p>
 <p><a class="asset-img-link" style="display: inline;"
-      href="<?php echo cdn("img/engine/android/10.0/guides/in-app/open-containing-folder.png"); ?>"><img
+      href="<?php echo cdn("img/engine/ios/14.0/guides/in-app/find-package-via-developer.png"); ?>"><img
         class="asset  asset-image at-xid-6a00d83432368053ef01bb07fd4d71970d image-full img-responsive"
         title="Keyman-developer-open-containing-folder"
-        src="<?php echo cdn("img/engine/android/10.0/guides/in-app/open-containing-folder-800wi.png"); ?>"
+        src="<?php echo cdn("img/engine/ios/14.0/guides/in-app/find-package-via-developer-800wi.png"); ?>"
         alt="Keyman-developer-open-containing-folder" border="0"/></a></p>
 <p><a class="asset-img-link" style="display: inline;"
-      href="<?php echo cdn("img/engine/android/10.0/guides/in-app/compiled-keyboard-file.png"); ?>"><img
+      href="<?php echo cdn("img/engine/ios/14.0/guides/in-app/package-file.png"); ?>"><img
         class="asset  asset-image at-xid-6a00d83432368053ef01b8d0e2f3e2970c image-full img-responsive"
         title="Compiled-keyboard-file"
-        src="<?php echo cdn("img/engine/android/10.0/guides/in-app/compiled-keyboard-file-800wi.png"); ?>"
+        src="<?php echo cdn("img/engine/ios/14.0/guides/in-app/package-file-800wi.png"); ?>"
         alt="Compiled-keyboard-file" border="0"/></a></p>
-<aside>
-  <h3>Tip</h3>
-  <p>You will need to append a keyboard version to the file. In this example, <b>tamil99m.js</b> has a
-    keyboard version 1.1, so it is renamed to <b>tamil99m-1.1.js</b>.</p>
-</aside>
-<h3>4. Add your keyboard to the project</h3>
-<p>Copy your compiled keyboard file (in this example <strong>tamil99m-1.1.js</strong>) to its own folder on your macOS machine
-  within the <strong>KMSample1\Keyboards</strong> folder. If you have a font, then place it within the same folder.
-  This example uses <strong>aava1.ttf</strong>.</p>
+<h3>4. Add your keyboard package to the project</h3>
+<p>Copy your compiled keyboard package (in this example <strong>ekwtamil99uni.kmp</strong>) to your macOS machine
+  within the <strong>KMSample1\Keyboards</strong> folder.
 <p><a class="asset-img-link" style="display: inline;"
-      href="<?php echo cdn("img/engine/ios/14.0/guides/in-app/keyboard-resource-finder.png"); ?>"><img
+      href="<?php echo cdn("img/engine/ios/14.0/guides/in-app/package-import.png"); ?>"><img
         class="asset  asset-image at-xid-6a00d83432368053ef01b8d0e3c836970c image-full img-responsive"
         title="Copy-keyboard-file"
-        src="<?php echo cdn("img/engine/ios/14.0/guides/in-app/keyboard-resource-finder-800wi.png"); ?>"
+        src="<?php echo cdn("img/engine/ios/14.0/guides/in-app/package-import.png"); ?>"
         alt="Copy-keyboard-file" border="0"/></a></p>
 <p>When you switch back into Xcode, use the right-click context menu to "Add Files to 'KMSample1'":</p>
 <p><a class="asset-img-link" style="display: inline;"
-      href="<?php echo cdn("img/engine/ios/14.0/guides/in-app/import-keyboard-in-xcode.png"); ?>"><img
+      href="<?php echo cdn("img/engine/ios/14.0/guides/in-app/xcode-package-import.png"); ?>"><img
         class="asset  asset-image at-xid-6a00d83432368053ef01b7c75abaa9970b img-responsive"
         title="Android-studio-assets"
-        src="<?php echo cdn("img/engine/ios/14.0/guides/in-app/import-keyboard-in-xcode-800wi.png"); ?>"
+        src="<?php echo cdn("img/engine/ios/14.0/guides/in-app/xcode-package-import-800wi.png"); ?>"
         alt="Android-studio-assets" border="0"/></a></p>
-<p>Next, edit <code>ViewController.viewDidLoad()</code> to add the keyboard (tamil99m) with <code>Manager.shared</code>
+<p>Next, edit <code>ViewController.viewDidLoad()</code> to add the keyboard (ekwtamil99uni) with <code>Manager.shared</code>
   from KeymanEngine.framework.
-  If you're not using a custom font (this example uses <strong>aava1.ttf</strong>), you can set KMKey_Font to the default font:</p>
 <pre class='language-swift'><code>  // Add a custom keyboard
-  let kb = InstallableKeyboard(id: "tamil99m",
-                               name: "Tamil 99M",
-                               languageID: "ta",
-                               languageName: "Tamil",
-                               version: "1.1",
-                               isRTL: false,
-                               font: Font(filename: "aava1.ttf"),
-                               oskFont: nil,
-                               isCustom: true)
-
-  let urls = [
-    Bundle.main.url(forResource: "tamil99m-1.1", withExtension: "js")!,
-    Bundle.main.url(forResource: "aava1", withExtension: "ttf")!
-  ]
+  let kmpFileURL = Bundle.main.url(forResource: "ekwtamil99uni", withExtension: "kmp")!
+  let keyboardID = FullKeyboardID(keyboardID: "ekwtamil99uni", languageID: "ta")
 
   do {
-    // Ensures the files are placed (at runtime) where the framework can easily manage them.
-    try Manager.shared.preloadFiles(forKeyboardID: kb.id, at: urls, shouldOverwrite: true)
+    let package = try ResourceFileManager.shared.prepareKMPInstall(from: kmpFileURL) as! KeyboardKeymanPackage
+    try ResourceFileManager.shared.install(resourceWithID: keyboardID, from: package)
+
+    _ = Manager.shared.setKeyboard(withFullID: keyboardID)
   } catch {
     print("Error preloading: \(error)")
   }
-
-  Manager.shared.addKeyboard(kb)
 </code></pre>
 <p><a class="asset-img-link" style="display: inline;"
-      href="<?php echo cdn("img/engine/ios/14.0/guides/in-app/keyboard-load-code.png"); ?>"><img
+      href="<?php echo cdn("img/engine/ios/14.0/guides/in-app/fully-imported.png"); ?>"><img
         class="asset  asset-image at-xid-6a00d83432368053ef01bb07fe335f970d image-full img-responsive"
         title="Android-studio-adding-keyboard"
-        src="<?php echo cdn("img/engine/ios/14.0/guides/in-app/keyboard-load-code-800wi.png"); ?>"
+        src="<?php echo cdn("img/engine/ios/14.0/guides/in-app/fully-imported-800wi.png"); ?>"
         alt="Android-studio-adding-keyboard" border="0"/></a></p>
 </code></pre>
 
@@ -139,12 +119,12 @@
 
 <p>You can run your app through Xcode's built-in Simulator, which can emulate a number of iOS devices.  Simply choose
 a target device to test against from the drop-down toward the top-left.  For example, in the previous image, you can see
-"iPhone X" selected.
+"iPod Touch (7th generation)" selected.
 <p><a class="asset-img-link" style="display: inline;"
       href="<?php echo cdn("img/engine/ios/14.0/guides/in-app/ios-simulator-tamil.png"); ?>"><img
         class="asset  asset-image at-xid-6a00d83432368053ef01b7c75abb95970b image-full img-responsive"
         title="Android-emulator-tamil"
-        src="<?php echo cdn("img/engine/ios/14.0/guides/in-app/ios-simulator-tamil-800wi.png"); ?>"
+        src="<?php echo cdn("img/engine/ios/14.0/guides/in-app/ios-simulator-tamil.png"); ?>"
         alt="Android-emulator-tamil" border="0"/></a></p>
 <p>And there you have it: your first Keyman Engine for iPhone and iPad app!</p>
 
