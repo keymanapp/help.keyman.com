831e295631c7a0451be1b0b66bf060bbbe0df4e7
Marc Durdin <marc@durdin.net>
2021-04-22 12:16:38 +1000

    fix: setRect parameter mismatch

--- developer/engine/web/14.0/reference/osk/setRect.php
+++ developer/engine/web/14.0/reference/osk/setRect.php
@@ -27,12 +27,13 @@ obj_spec("setRect",
   "top", "optional number", "Sets the y-coordinate of the OSK's top side.",
   "width", "optional number", "Sets the width of the OSK.",
   "height", "optional number", "Sets the height of the OSK.",
-  "nosize", "optional string", "If set to <code>'none'</code>, prevents the user from altering the OSK's size.  ".
-    "Setting the value to <code>'block'</code> will undo this.",
-  "nomove", "optional string", "If set to <code>'none'</code>, prevents the user from relocating the OSK manually.  ".
-    "Setting the value to <code>'block'</code> will undo this."
+  "nosize", "optional boolean", "If set to <code>true</code>, prevents the user from altering the OSK's size. If set to <code>false</code>, allows the user to resize the OSK. If omitted, does not change the existing state.",
+  "nomove", "optional boolean", "If set to <code>true</code>, prevents the user from relocating the OSK manually. If set to <code>false</code>, allows the user to move the OSK. If omitted, does not change the existing state."
 );
 ?>
 
 <p>The values and states of omitted object members will not be changed.
 </p>
+
+<p>This differs from <code>setPos()</code> in that it allows resizing the keyboard and controlling the user's ability to move/size the keyboard. Furthermore, when
+<code>left</code> and <code>top</code> members are included, they override the default position of the keyboard rather than setting the position temporarily.</p>
