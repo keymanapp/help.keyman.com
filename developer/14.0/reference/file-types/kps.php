d96edd4b5a4a17144317a404c0eacd260b9fbea0
Darcy Wong <darcy_wong@sil.org>
2021-04-30 12:40:26 +0700

    fix: Reference strings.xml file and localization

--- developer/14.0/reference/file-types/kps.php
+++ developer/14.0/reference/file-types/kps.php
@@ -26,37 +26,7 @@
 <h2>File format</h2>
 
 <p>The optional <code class='language-markup'>&lt;Strings></code> section of the file can be included to customise the text in the bootstrap installer. The default
-strings are shown below:</p>
-<pre class='language-markup'><code>&lt;Strings>
-  &lt;String Name="ssFontSize_Dialog" Value="16"/>
-  &lt;String Name="ssFontSize_Title" Value="24"/>
-  &lt;String Name="ssFontSize_InstallButton" Value="18"/>
-  &lt;String Name="ssFontName_Dialog" Value=""/>
-  &lt;String Name="ssFontName_Title" Value=""/>
-  &lt;String Name="ssFontName_InstallButton" Value=""/>
-  &lt;String Name="ssApplicationTitle" Value="Keyman Developer Setup"/>
-  &lt;String Name="ssTitle" Value="%0:s Setup"/>
-  &lt;String Name="ssWelcome_Plain" Value="Setup will install %0:s"/>
-  &lt;String Name="ssSIL" Value="Created by SIL International"/>
-  &lt;String Name="ssCheckForUpdates" Value="Check for &amp;amp;updates online before installing"/>
-  &lt;String Name="ssInstallButton" Value="&amp;amp;Install Keyman Developer"/>
-  &lt;String Name="ssExitButton" Value="E&amp;amp;xit"/>
+  strings are found in the file:
+  <a href="https://github.com/keymanapp/keyman/blob/stable-14.0/windows/src/desktop/setup/locale/en/strings.xml">strings.xml</a>
+  and can be localized at <a href="https://translate.keyman.com">translate.keyman.com</a></p>
 
-  &lt;String Name="ssErrorDownloadingUpdate" Value="Error %0:d downloading update from server"/>
-  &lt;String Name="ssErrorUnableToContactServer" Value="Unable to contact server"/>
-  &lt;String Name="ssErrorUnableToContactServerDetailed" Value="Unable to contact server, error was: %0:s"/>
-
-  &lt;String Name="ssStatusCheckingForUpdates" Value="Checking for updates online"/>
-  &lt;String Name="ssStatusInstalling" Value="Installing Keyman Developer"/>
-  &lt;String Name="ssStatusComplete" Value="Installation Complete"/>
-
-  &lt;String Name="ssQueryUpdateVersion" Value="Version %1:s of Keyman Developer has been released and is available for download.
-   This update is %0:dKB. Do you want to download and install the updated version (recommended)?"/>
-
-  &lt;String Name="ssQueryRestart" Value="You must restart Windows before Setup can complete.  When you restart Windows, Setup will continue.  Restart now?"/>
-  &lt;String Name="ssErrorUnableToAutomaticallyRestart" Value="Windows was not able to be automatically restarted.  You should restart Windows before you try and start Keyman."/>
-
-  &lt;String Name="ssMustRestart" Value="You must restart Windows to complete Setup.  When you restart Windows, Setup will finish."/>
-  &lt;String Name="ssCheckForUpdatesError" Value="The online update check failed with an error: %0:s"/>
-&lt;/Strings>
-</code></pre>
