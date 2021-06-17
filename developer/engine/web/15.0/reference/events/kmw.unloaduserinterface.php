12880dafb71552cc677cd5dcd6198d27732d7cb1
Darcy Wong <darcy_wong@sil.org>
2021-06-17 14:30:41 +0700

    chore: Seed KeymanWeb engine content for 15.0

---
+++ developer/engine/web/15.0/reference/events/kmw.unloaduserinterface.php
@@ -0,0 +1,33 @@
+<?php
+  require_once('includes/template.php');
+
+  head([
+    'title' => "kmw.unloaduserinterface Event"
+  ]);
+
+  require_once('includes/__docheader.php');
+?>
+
+<h1 class="title" id="event_kmw.unloaduserinterface">kmw.unloaduserinterface Event</h1>
+
+<h2 id="Summary" name="Summary">Summary</h2>
+
+<p>Called when allow ui clean-up.</p>
+
+<?php
+syntax(<<<END
+keyman.addEventListener('unloaduserinterface', function() {
+  ...
+});
+END
+);
+
+params();
+returns("boolean", "<code>true</code> if the event should continue processing, <code>false</code> if it should not. \n".
+  "Your event handler should return <code>true</code> aside from exceptional circumstances.");
+?>
+
+<h2 id="Description" name="Description">Description</h2>
+
+<p>Called when the ui is to be unloaded, allowing cleanup of resources if necessary.
+</p>
