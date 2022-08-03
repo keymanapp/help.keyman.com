039a075465f6c01801522e43d66cc94ca02c5010
Ross <writetoross@gmail.com>
2022-08-03 15:27:35 +1000

    docs(core): add api calls introduced v15
    Added structures api calls and new memebers added
    to the API in version 15

--- developer/core/desktop/15.0/c-api/keyboard-api.php
+++ developer/core/desktop/15.0/c-api/keyboard-api.php
@@ -58,6 +58,66 @@ typedef struct {
   </section>
 </article>
 
+<article id="km_kbp_keyboard_key">
+  <h1>km_kbp_keyboard_key</h1>
+  <section class="description">
+    <h2>Description</h2>
+    <p>A structure pairing a virtual key and modifier</p>
+  </section>
+  <section class="specification">
+    <h2>Specification</h2>
+<pre><code class="language-c">
+typedef struct {
+  km_kbp_virtual_key key;
+  uint32_t modifier_flag;
+} km_kbp_keyboard_key;
+</code></pre>
+  </section>
+  <section class="members">
+    <h2>Members</h2>
+    <dl>
+      <dt id="key"><code class="token symbol">key</code></dt>
+      <dd> a virtual key code </dd>
+
+      <dt id="modifier_flag"><code class="token symbol">modifier_flag</code></dt>
+      <dd>The modifier flag </dd>
+
+    </dl>
+  </section>
+</article>
+
+<article id="km_kbp_keyboard_imx">
+  <h1>km_kbp_keyboard_imx</h1>
+  <section class="description">
+    <h2>Description</h2>
+    <p>A structure of IMX library and function name associated with an unique id</p>
+  </section>
+  <section class="specification">
+    <h2>Specification</h2>
+<pre><code class="language-c">
+typedef struct {
+  km_kbp_cp const * library_name;
+  km_kbp_cp const * function_name;
+  uint32_t imx_id;
+} km_kbp_keyboard_imx;
+
+</code></pre>
+  </section>
+  <section class="members">
+    <h2>Members</h2>
+    <dl>
+      <dt id="library_name"><code class="token symbol">library_name</code></dt>
+      <dd> IMX library name </dd>
+
+      <dt id="function_name"><code class="token symbol">function_name</code></dt>
+      <dd> IMX function name  </dd>
+      
+      <dt id="imx_id"><code class="token symbol">imx_id</code></dt>
+      <dd> unique identifier used to call this function </dd>
+
+    </dl>
+  </section>
+</article>
 
 <article id="km_kbp_keyboard_load">
   <h1>km_kbp_keyboard_load</h1>
@@ -185,6 +245,141 @@ km_kbp_keyboard_get_attrs(km_kbp_keyboard const *keyboard,
 
       <dt id="KM_KBP_STATUS_INVALID_ARGUMENT"><code class="token constant">KM_KBP_STATUS_INVALID_ARGUMENT</code></dt>
       <dd>If non-optional parameters are null.</dd>
+    </dl>
+  </section>
+</article>
+
+<article id="km_kbp_keyboard_get_key_list">
+  <h1>km_kbp_keyboard_get_key_list</h1>
+  <section class="description">
+    <h2>Description</h2>
+    <p>
+    Returns the unordered full set of modifier + virtual keys that are handled by the
+keyboard. The matching dispose call needs to be called to free the memory.
+    </p>
+  </section>
+  <section class="specification">
+    <h2>Specification</h2>
+<pre><code class="language-c">km_kbp_status
+km_kbp_keyboard_get_key_list(km_kbp_keyboard const *keyboard,
+                            km_kbp_keyboard_key const **out);
+
+</code></pre>
+  </section>
+  <section class="parameters">
+    <h2>Parameters</h2>
+    <dl>
+      <dt id="keyboard"><code class="token symbol">keyboard</code></dt>
+      <dd>A pointer to the opaque keyboard object to be queried.</dd>
+
+      <dt id="out"><code class="token symbol">out</code></dt>
+      <dd>
+        A pointer to the result, an array of 
+        <a href="#km_kbp_keyboard_key"><code class="token symbol">km_kbp_keyboard_keys</code></a>
+      </dd>
+    </dl>
+  </section>
+  <section class="returns">
+    <h2>Returns</h2>
+    <dl>
+      <dt id="KM_KBP_STATUS_OK"><code class="token constant">KM_KBP_STATUS_OK</code></dt>
+      <dd>On success.</dd>
+
+      <dt id="KM_KBP_STATUS_INVALID_ARGUMENT"><code class="token constant">KM_KBP_STATUS_INVALID_ARGUMENT</code></dt>
+      <dd>If non-optional parameters are null.</dd>
+    </dl>
+  </section>
+</article>
+
+<article id="km_kbp_keyboard_key_list_dispose">
+  <h1>km_kbp_keyboard_key_list_dispose</h1>
+  <section class="description">
+    <h2>Description</h2>
+    <p>
+    Free the allocated memory belonging to a keyboard key list previously
+    returned by <a href="#km_kbp_keyboard_get_key_list"><code class="token symbol">km_kbp_keyboard_get_key_list</code></a>      
+    </p>
+  </section>
+  <section class="specification">
+    <h2>Specification</h2>
+<pre><code class="language-c">void km_kbp_keyboard_key_list_dispose(km_kbp_keyboard_key *key_list);
+
+</code></pre>
+  </section>
+  <section class="parameters">
+    <h2>Parameters</h2>
+    <dl>
+      <dt id="keyboard"><code class="token symbol">keyboard</code></dt>
+      <dd>A pointer to the keyboard key list to be
+    disposed of.</dd>
+    </dl>
+  </section>
+</article>
+
+<article id="km_kbp_keyboard_get_imx_list">
+  <h1>km_kbp_keyboard_get_imx_list</h1>
+  <section class="description">
+    <h2>Description</h2>
+    <p>
+    Returns the list of IMX libraries and function names that are referenced by
+    the keyboard. The matching dispose call needs to be called to free the memory.
+    </p>
+  </section>
+  <section class="specification">
+    <h2>Specification</h2>
+<pre><code class="language-c">km_kbp_status km_kbp_keyboard_get_imx_list(
+                              km_kbp_keyboard const *keyboard, 
+                              km_kbp_keyboard_imx** imx_list);
+
+</code></pre>
+  </section>
+  <section class="parameters">
+    <h2>Parameters</h2>
+    <dl>
+      <dt id="keyboard"><code class="token symbol">keyboard</code></dt>
+      <dd>A pointer to the opaque keyboard object to be queried.</dd>
+
+      <dt id="imx_list"><code class="token symbol">imx_list</code></dt>
+      <dd>
+        A pointer to the result, an array of 
+        <a href="#km_kbp_keyboard_imx"><code class="token symbol">km_kbp_keyboard_imx</code></a>
+        structure.
+      </dd>
+    </dl>
+  </section>
+  <section class="returns">
+    <h2>Returns</h2>
+    <dl>
+      <dt id="KM_KBP_STATUS_OK"><code class="token constant">KM_KBP_STATUS_OK</code></dt>
+      <dd>On success.</dd>
+
+      <dt id="KM_KBP_STATUS_INVALID_ARGUMENT"><code class="token constant">KM_KBP_STATUS_INVALID_ARGUMENT</code></dt>
+      <dd>If non-optional parameters are null.</dd>
+    </dl>
+  </section>
+</article>
+
+<article id="km_kbp_keyboard_imx_list_dispose">
+  <h1>km_kbp_keyboard_imx_list_dispose</h1>
+  <section class="description">
+    <h2>Description</h2>
+    <p>
+    Free the allocated memory belonging to a IMX list previously
+returned by <a href="#km_kbp_keyboard_imx_list_dispose"><code class="token symbol">km_kbp_keyboard_imx_list_dispose</code></a>.
+    </p>
+  </section>
+  <section class="specification">
+    <h2>Specification</h2>
+<pre><code class="language-c">void km_kbp_keyboard_key_list_dispose(km_kbp_keyboard_key *key_list);
+
+</code></pre>
+  </section>
+  <section class="parameters">
+    <h2>Parameters</h2>
+    <dl>
+      <dt id="keyboard"><code class="token symbol">keyboard</code></dt>
+      <dd>A pointer to the IMX list to be
+    disposed of.</dd>
     </dl>
   </section>
 </article>
