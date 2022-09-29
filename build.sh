961052db34d17ffde3f6fb53eeecf4d3a7c96202
Darcy Wong <darcy_wong@sil.org>
2022-09-29 10:40:43 +0700

    chore: Rename image name

--- build.sh
+++ build.sh
@@ -31,34 +31,43 @@ if builder_start_action configure; then
 fi
 
 if builder_start_action clean; then
-  # Stop and cleanup docker containers using image:keyman-websites
-  HELP_CONTAINERS=$(docker ps -a -q --filter ancestor=keyman-websites)
+  # Stop and cleanup docker containers using image:help-keyman-website
+  HELP_CONTAINERS=$(docker ps -a -q --filter ancestor=help-keyman-website)
   docker container stop $HELP_CONTAINERS
   docker container rm $HELP_CONTAINERS
 
-  # Cleanup keyman-websites image
-  docker rmi keyman-websites
+  # Cleanup help-keyman-website image
+  docker rmi help-keyman-website
 
   builder_finish_action success clean
 fi
 
 if builder_start_action build; then
   # Download docker image
-  docker build -t keyman-websites .
+  docker build -t help-keyman-website .
 
   if [[ $OSTYPE =~ mysys|cygwin ]]; then
     # Windows needs leading slashes for path
-    docker run -d -p 8055:80 -v //$(pwd):/var/www/html/ -e S_KEYMAN_COM=localhost:8054 keyman-websites
+    docker run -d -p 8055:80 -v //$(pwd):/var/www/html/ -e S_KEYMAN_COM=localhost:8054 help-keyman-website
   else
-    docker run -d -p 8055:80 -v $(pwd):/var/www/html/ -e S_KEYMAN_COM=localhost:8054 keyman-websites
+    docker run -d -p 8055:80 -v $(pwd):/var/www/html/ -e S_KEYMAN_COM=localhost:8054 help-keyman-website
   fi
 
   builder_finish_action success build
 fi
 
 if builder_start_action test; then
-  # TODO Lint?
 
-  # Check for broken links
-  composer check-docker-links
+  LINT_STATUS=$(_test_lint)
+  echo "_test_lint done with status $LINT_STATUS"
+  if [ "${LINT_STATUS}" -ne 0 ]; then
+    build_finish_action fail test
+  fi  
+  #composer lint
+
+  #_test_broken_links
+  #composer check-docker-links
+  #echo "_test_broken_links done"
+
+  builder_finish_action success test
 fi
