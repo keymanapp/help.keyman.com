7c75ff8551de4c9d4cbed9e00c05452dc3dced38
Marc Durdin <marc@durdin.net>
2023-09-27 06:32:24 +0700

    chore: cleanup build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-#
-# Setup help.keyman.com site to run via Docker.
-#
-set -eu
-
 ## START STANDARD BUILD SCRIPT INCLUDE
 # adjust relative paths as necessary
 readonly THIS_SCRIPT="$(readlink -f "${BASH_SOURCE[0]}")"
@@ -11,6 +6,7 @@ readonly BOOTSTRAP="$(dirname "$THIS_SCRIPT")/resources/bootstrap.inc.sh"
 [ -f "$BOOTSTRAP" ] && source "$BOOTSTRAP" || source <(curl -fs https://raw.githubusercontent.com/keymanapp/shared-sites/main/bootstrap.inc.sh?token=$(date +%s))
 ## END STANDARD BUILD SCRIPT INCLUDE
 
+# TODO: these should probably all be moved to a common defines script too?
 readonly HELP_CONTAINER_NAME=help-keyman-website
 readonly HELP_CONTAINER_DESC=help-keyman-com-app
 readonly HELP_IMAGE_NAME=help-keyman-website
@@ -43,4 +39,4 @@ builder_run_action stop       stop_docker_container  $HELP_IMAGE_NAME $HELP_CONT
 builder_run_action build      build_docker_container $HELP_IMAGE_NAME $HELP_CONTAINER_NAME
 builder_run_action start      start_docker_container $HELP_IMAGE_NAME $HELP_CONTAINER_NAME $HELP_CONTAINER_DESC $HOST_HELP_KEYMAN_COM $PORT_HELP_KEYMAN_COM
 
-builder_run_action test       test_docker_container  #$IMAGE_NAME $CONTAINER_NAME
+builder_run_action test       test_docker_container
