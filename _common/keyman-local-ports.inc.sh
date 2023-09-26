9ca0a472cfedaf46e8674ec699d2594051e4e9ce
Marc Durdin <marc@durdin.net>
2023-09-26 13:22:42 +0700

    refactor: split common parts out of build.sh

---
+++ _common/keyman-local-ports.inc.sh
@@ -0,0 +1,7 @@
+# This file lists default local host ports for all keyman.com.localhost sites
+
+readonly PORT_KEYMAN_COM=8053
+readonly PORT_S_KEYMAN_COM=8054
+readonly PORT_HELP_KEYMAN_COM=8055
+readonly PORT_KEYMANWEB_COM=8057
+readonly PORT_API_KEYMAN_COM=8058
--- build.sh
+++ build.sh
@@ -10,24 +10,18 @@ THIS_SCRIPT="$(greadlink -f "${BASH_SOURCE[0]}" 2>/dev/null || readlink -f "${BA
 . "$(dirname "$THIS_SCRIPT")/resources/builder.inc.sh"
 ## END STANDARD BUILD SCRIPT INCLUDE
 
+readonly HELP_CONTAINER_NAME=help-keyman-website
+readonly HELP_CONTAINER_DESC=help-keyman-com-app
+readonly HELP_IMAGE_NAME=help-keyman-website
+readonly HOST_HELP_KEYMAN_COM=help.keyman.com.localhost
+
+. "$REPO_ROOT/_common/keyman-local-ports.inc.sh"
+. "$REPO_ROOT/_common/docker.inc.sh"
+
+# This script runs from its own folder
+cd "$REPO_ROOT"
+
 ################################ Main script ################################
-
-function _get_docker_image_id() {
-  echo "$(docker images -q help-keyman-website)"
-}
-
-function _get_docker_container_id() {
-  echo "$(docker ps -a -q --filter ancestor=help-keyman-website)"
-}
-
-function _stop_docker_container() {
-  HELP_CONTAINER=$(_get_docker_container_id)
-  if [ ! -z "$HELP_CONTAINER" ]; then
-    docker container stop $HELP_CONTAINER
-  else
-    echo "No Docker container to stop"
-  fi
-}
 
 builder_describe \
   "Setup help.keyman.com site to run via Docker." \
@@ -40,87 +34,15 @@ builder_describe \
 
 builder_parse "$@"
 
-# This script runs from its own folder
-cd "$REPO_ROOT"
-
-if builder_start_action configure; then
-  # Nothing to do
-  builder_finish_action success configure
-fi
-
-if builder_start_action clean; then
-  # Stop and cleanup Docker containers and images used for the site
-  _stop_docker_container
-
-  HELP_CONTAINER=$(_get_docker_container_id)
-  if [ ! -z "$HELP_CONTAINER" ]; then
-    docker container rm $HELP_CONTAINER
-  else
-    echo "No Docker container to clean"
-  fi
-    
-  HELP_IMAGE=$(_get_docker_image_id)
-  if [ ! -z "$HELP_IMAGE" ]; then
-    docker rmi help-keyman-website
-  else 
-    echo "No Docker image to clean"
-  fi
-
-  builder_finish_action success clean
-fi
-
-if builder_start_action stop; then
-  # Stop the Docker container
-  _stop_docker_container
-  builder_finish_action success stop
-fi
-
-if builder_start_action build; then
-  # Download docker image. --mount option requires BuildKit  
-  DOCKER_BUILDKIT=1 docker build -t help-keyman-website .
-
-  builder_finish_action success build
-fi
-
-if builder_start_action start; then
-  # Start the Docker container
-  if [ ! -z $(_get_docker_image_id) ]; then
-    if [[ $OSTYPE =~ msys|cygwin ]]; then
-      # Windows needs leading slashes for path
-      SITE_HTML="//$(pwd):/var/www/html/"
-    else
-      SITE_HTML="$(pwd):/var/www/html/"
-    fi
-    
-    docker run --rm -d -p 8055:80 -v ${SITE_HTML} \
-      -e S_KEYMAN_COM=localhost:8054 \
-      --name help-keyman-com-app \
-      help-keyman-website
-  else
-    echo "${COLOR_RED}ERROR: Docker container doesn't exist. Run ./build.sh build first${COLOR_RESET}"
-    builder_finish_action fail start
-  fi
-
-  # Skip if link already exists
-  if [ -L vendor ]; then
-    echo "Link to vendor/ already exists"
-  else
-    # Create link to vendor/ folder
-    HELP_CONTAINER=$(_get_docker_container_id)
-    if [ ! -z "$HELP_CONTAINER" ]; then
-      docker exec -i $HELP_CONTAINER sh -c "ln -s /var/www/vendor vendor && chown -R www-data:www-data vendor"
-    else
-      echo "No Docker container not running to create link to vendor/"
-    fi
-  fi
-
-  builder_finish_action success start
-fi
-
-if builder_start_action test; then
+function test_docker_container() {
   # TODO: lint tests
-
   composer check-docker-links
+}
 
-  builder_finish_action success test
-fi
+# builder_run_action configure echo "configure - nothing to do"
+builder_run_action clean clean_docker_container $HELP_IMAGE_NAME $HELP_CONTAINER_NAME
+builder_run_action stop  stop_docker_container  $HELP_IMAGE_NAME $HELP_CONTAINER_NAME
+builder_run_action build build_docker_container $HELP_IMAGE_NAME $HELP_CONTAINER_NAME
+builder_run_action start start_docker_container $HELP_IMAGE_NAME $HELP_CONTAINER_NAME $HELP_CONTAINER_DESC $HOST_HELP_KEYMAN_COM $PORT_HELP_KEYMAN_COM
+
+builder_run_action test  test_docker_container  #$IMAGE_NAME $CONTAINER_NAME
