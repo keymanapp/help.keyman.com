862e1e9b0b561ffc1f4387e78f48b7240b044638
Marc Durdin <marc@durdin.net>
2023-09-26 11:59:36 +0700

    chore: seed Developer 17.0 documentation on staging

---
+++ developer/17.0/guides/distribute/tutorial/step-5.php
@@ -0,0 +1,53 @@
+<?php
+  require_once('includes/template.php');
+
+  head([
+    'title' => "Step 5: Shortcuts",
+    'css' => ['template.css','kmguides.css']
+  ]);
+?>
+
+<h1>Step 5: Shortcuts</h1>
+
+<p>Click on the Shortcuts tab in the Package Editor.</p>
+
+<p><img src="<?php echo cdn("img/developer/100/tutorial_distribute_keyboard_shortcuts.png"); ?>" /></p>
+
+<p>While it may be a good idea to include Start Menu shortcuts for your package, on recent versions of Windows these are not very visible
+to end users. This means that you should ensure that all instructions are available through the <strong>welcome.htm</strong> file as well.
+The Start Menu shortcuts were designed originally for Windows-based keyboard packages and work slightly differently in macOS.</p>
+
+<p>You should consider adding the following items to the Start Menu:</p>
+
+<ul type="disc">
+  <li>Documentation shortcuts</li>
+  <li>Welcome.htm shortcut</li>
+</ul>
+
+<aside>
+  <h3>Note</h3>
+
+  <p>While Keyman Developer 8.0 and earlier versions of the documentation advised including an uninstall shortcut to the package, this is no longer recommended.
+  Users should uninstall your package from Keyman Configuration. Adding an uninstall shortcut to the package adds confusion to the
+  Start Menu search on Windows, and is not compatible with macOS keyboards.</p>
+</aside>
+
+<p>Click <span class="guibutton">New</span> to add a new shortcut to the package. Enter a description for the shortcut, and select the
+program or file to start from the Program list. Four predefined program entries (Start Product), (Product Configuration), (Product
+Help) and (About Product) will be translated into the appropriate shortcuts to start Keyman Desktop tasks as described.</p>
+
+<aside>
+  <h3>Note</h3>
+
+  <p>In Keyman 6 and earlier, the predefined targets $KEYMAN\keyman.exe and $KEYMAN\kmshell.exe were available. These are
+  translated to (Start Product) and (Product Configuration), respectively, in Keyman Desktop 7.0 and later versions.</p>
+</aside>
+
+<aside>
+  <h3>Tip</h3>
+
+  <p>Packages are not listed in the Control Panel Add/Remove Programs applet with Keyman Desktop 7.0 and later versions. Packages
+  can be uninstalled through the Start Menu shortcut or from Keyman Desktop Configuration.</p>
+</aside>
+
+<p><a href="step-6" title="Step 6: Compiling, testing and distributing a Package">Step 6: Compiling, testing and distributing a Package</a></p>
---
+++ developer/17.0/guides/lexical-models/distribute/tutorial/index.php
@@ -0,0 +1,34 @@
+<?php
+  require_once('includes/template.php');
+
+  head([
+    'title' => "Lexical Model Package Tutorial"
+  ]);
+?>
+
+<h1>Lexical Model Package Tutorial</h1>
+
+<dl>
+  <dt><a href="step-1">Step 1: What do we include?</a></dt>
+  <dt><a href="step-2">Step 2: Editing .htm files for the Package</a></dt>
+  <dt><a href="step-3">Step 3: Checking a package and adding files</a></dt>
+  <dt><a href="step-4">Step 4: Filling in package details</a></dt>
+  <dt><a href="step-5">Step 5: Compiling, testing and distributing a Package</a></dt>
+</dl>
+
+<h2>Overview</h2>
+
+<p>Welcome! In this tutorial, you will learn how to create a
+lexical model <a href="../../../../reference/file-types/kmp" title="KMP files">package</a>. A
+package is a collection of files, compressed into a single file, just like a ZIP file. It is designed to make installation of
+a lexical model straightforward for the end user. When done, Keyman for mobile apps can download and install
+lexical models through the package.</p>
+
+<p>We will be creating a package for the SENĆOŦEN lexical model that was made in
+  <a href="../../tutorial/" title="Developing a lexical model from a word list">Developing a lexical model from a word list</a>.</p>
+
+<h3>Let's begin</h3>
+
+<p>Let's get started! Move on to the next topic to begin the first step, choosing what to include in the package.</p>
+
+<p><a href="step-1" title="Step 1: What do we include?">Step 1: What do we include?</a></p>
