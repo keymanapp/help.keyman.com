8f1e96c181d5094e433bc1be40ae41052e30ce9b
Marc Durdin <marc@durdin.net>
2023-10-16 14:57:54 +0700

    chore: split keyboard index cache build

--- keyboard/index.php
+++ keyboard/index.php
@@ -2,47 +2,7 @@
   require_once('includes/template.php');
 
   define('INDEX_CACHE', 'index.cache');
-
-  if(!file_exists(INDEX_CACHE)) {
-    $dirs = array_filter(glob('*'), 'is_dir');
-    $items = array('active' => array(), 'deprecated' => array());
-    foreach($dirs as $directory){
-      $version = '1.0';
-      $versions = array_filter(glob($directory . '/*'), 'is_dir');
-      if(count($versions) == 0) continue;
-      //var_dump($versions);
-      $filename = array_pop($versions).'/'.$directory.'.php';
-      if(!file_exists($filename)) continue;
-      $s = @file_get_contents($filename);
-
-      if(preg_match('/\$pagename\s*=\s*[\'"](.*)[\'"]\s*;/', $s, $sub)){
-        $title = $sub[1];
-      } else if(preg_match('/\$pagetitle\s*=\s*[\'"](.*)[\'"]\s*;/', $s, $sub)){
-        $title = $sub[1];
-      } else {
-        continue; // not a keyboard help file
-      }
-
-      if(substr($title,0,1) == '$') {
-        continue;
-      }
-
-      if(preg_match('/^(.*)\s+Keyboard\s+Help/i', $title, $sub)) {
-        $title = stripslashes($sub[1]);
-      }
-
-      $dep = strpos($title, "(deprecated)");
-      if($dep !== false) {
-        $items['deprecated'][$directory] = trim(substr($title, 0, $dep));
-      } else {
-        $items['active'][$directory] = $title;
-      }
-    }
-
-    file_put_contents(INDEX_CACHE, json_encode($items));
-  } else {
-    $items = json_decode(file_get_contents(INDEX_CACHE), true);
-  }
+  $items = json_decode(file_get_contents(INDEX_CACHE), true);
 
   // Required
   head([
