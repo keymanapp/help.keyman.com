73d975f41b70ff618e61d0c6acd37780938cb507
Keyman Build Server <keyman-server@users.noreply.github.com>
2024-07-30 15:52:19 -0400

    auto: Keyboard help deployment

---
+++ keyboard/fv_henqeminem/10.2/fv_henqeminem.php
@@ -0,0 +1,114 @@
+<?php 
+  $pagename = 'hǝn̓q̓ǝmin̓ǝm̓ Keyboard Help';
+  $pagetitle = 'hǝn̓q̓ǝmin̓ǝm̓ Keyboard Help';
+  $pagestyle = <<<END
+  
+#wikitext table { margin: 12px; }
+#wikitext table td { border: solid 1px #888888 !important }
+#wikitext table th { border: solid 1px #888888 !important; background: #ffcccc }
+.lang2 {font-family:"Aboriginal Sans"}  
+END;
+
+  require_once('header.php');
+?>
+
+<p style='margin: 16px 0 0 0'>
+This keyboard is designed for the <b>hǝn̓q̓ǝmin̓ǝm̓</b> language of the BC Coast region of Canada.
+</p>
+<p>If square boxes are displayed instead of characters when using this keyboard (and in the keyboard layouts below), please read our <a href="/troubleshooting/#boxes">troubleshooting guide</a>.
+</p>
+
+<img src="Henqeminem_U_.png" alt="" height="198" width="514" border="0" /></h2>
+	<ul>
+			<li>The above keymap shows the special characters that have been assigned to the number keys. To type numerals, hold down the Right-Alt or Option key then press the appropriate number key. The Numeric keypad on the right side of many keyboards keeps its original numbers.</li>
+			<li>All accents are typed after the base character: t̓ is typed t then Apostrophe '
+				<ul>
+					<li>To type the glottalized accent use the Apostrophe key <kbd>'</kbd> — k̓ is typed <kbd>k</kbd> then Apostrophe <kbd>'</kbd></li>
+					<li>To type the acute accent use the Semicolon key <kbd>;</kbd> — á is typed <kbd>a</kbd> then Semicolon <kbd>;</kbd></li>
+					<li>To type the háček accent use the Shift+Backquote key — č is typed c then Shift+Backquote<br />
+								The letters č, š, x̌ also have their own keys: <kbd>5</kbd>, <kbd>6</kbd>, <kbd>7</kbd>.</li>
+					<li>To type the grave accent use the Backquote key <kbd>`</kbd> — à is typed <kbd>a</kbd> then Backquote <kbd>`</kbd></li>
+					<li>To type the <strong>belted-ɬ</strong> press the <kbd>9</kbd> key.</li>
+				</ul></li>
+				<li>All changed punctuation can type their original value by holding down the <kbd>Right-Alt</kbd> key (Win) or <kbd>Option</kbd> key (Mac). <br />
+							The Semicolon <kbd>;</kbd> is typed <kbd>Right-Alt</kbd>+<kbd>;</kbd> (Win) or <kbd>Option</kbd>+<kbd>;</kbd> (Mac).</li>
+	</ul>				 
+	<ul>
+				<li>There are several additional characters which can be typed holding down the Right-Alt or Option key.</li>
+					<ul>
+						<li>The <strong>chi-χ</strong> is typed <kbd>Right-Alt</kbd>+<kbd>x</kbd> (Windows) or <kbd>Option</kbd>+<kbd>x</kbd> (Mac).</li>
+						<li>The <strong>barred-ł</strong> is typed <kbd>Right-Alt</kbd>+<kbd>l</kbd> (Win) or <kbd>Option</kbd>+<kbd>l</kbd> (Mac).</li>
+						<li> The <strong>middle dot ·</strong> is typed <kbd>Right-Alt</kbd>+<kbd>.</kbd> (Win) or <kbd>Option</kbd>+<kbd>.</kbd> (Mac). </li>
+						<li> The <strong>raised theta ᶿ</strong> is typed <kbd>Right-Alt</kbd>+<kbd>t</kbd> (Win) or <kbd>Option</kbd>+<kbd>t</kbd> (Mac). </li>
+					</ul>
+				<li>Opening and closing quotes. For Mac users, Right-Alt is either of the Option Keys.</li>
+					<ul>
+						<li>single: ‘ Left-Bracket <kbd>[</kbd> — ’ Right-Bracket <kbd>]</kbd></li>
+						<li>double: “ <kbd>Shift</kbd>+<kbd>[</kbd> — ” <kbd>Shift</kbd>+<kbd>]</kbd></li>
+						<li>single ‹ <kbd>Right-Alt</kbd>+<kbd>Shift</kbd>+<kbd>9</kbd> — › <kbd>Right-Alt</kbd>+<kbd>Shift</kbd>+<kbd>0</kbd></li>
+						<li>double « <kbd>Right-Alt</kbd>+<kbd>9</kbd> — » <kbd>Right-Alt</kbd>+<kbd>0</kbd></li>
+					</ul>
+	</ul>
+	
+<h2>Unicode Fonts</h2>
+	<p>The following fonts have been installed on your computer: <strong>Aboriginal Sans, Aboriginal Serif.</strong> Please use these fonts when typing in your language as they have been specially built to represent your language as accurately as possible. </p>
+<h3><a href="http://www.languagegeek.com/font/fontdownload.html">Download the latest version of Aboriginal Sans and Aboriginal Serif</a></h3>
+	
+	
+<h2>Mobile Keyboard Layout</h2>
+<div id=‘osk-tablet’ data-states=‘default shift’></div>
+
+<p>Long-press options are available. The first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.</p>
+
+<div id='wikitext'>
+
+<table border='1' style='border-collapse:collapse' cellpadding='5' >
+	<tr align="center">
+		<td>q q̓</td>
+		<td>w w̓</td>
+		<td>e é</td>
+		<td>ə ə́</td>
+		<td>t t̓</td>
+		<td>y y̓</td>
+		<td>u ú</td>
+		<td>i í</td>
+		<td>o</td>
+		<td>p p̓</td>
+	</tr>
+	<tr  align="center">
+		<td>a á</td>
+		<td>s</td>
+		<td>ʔ</td>
+		<th>’</th>
+		<td>θ t̓ᶿ</td>
+		<td>h</td>
+		<td>ʷ</td>
+		<td>k k̓</td>
+		<td>l l̓ ɬ</td>
+		<td>ƛ̓</td>
+	</tr>
+	<tr  align="center">
+		<td >⇧</td>
+		<td>χ</td>
+		<td>x x̌</td>
+		<td>c c̓</td>
+		<td>č</td>
+		<td>š</td>
+		<td>n n̓</td>
+		<td>m m̓</td>
+		<td  colspan='2'>⌫</td>
+	</tr>
+	<tr align="center">
+		<td>123</td><td>🌐</td><td>:</td><td colspan="4">space</td><td>.</td><td colspan="2">⏎</td>
+	</tr>
+	
+	</tr>
+</table>
+
+<div class='vspace'></div>
+<ul>
+	<li>Keys with a red background are combining accents. On your mobile device, they will have green text.</li>
+	<li>The first character in a cell is the "one-tap" key, any further characters are "hold-select" keys.</li>
+	<li>Common punctuation marks can be found by pressing and holding the period key on the right side of the space bar.</li>
+</ul>
+<div class='vspace'></div>
