b3b55c70a752e690eb907f70c9de727654977dd5
Keyman Build Server <keyman-server@users.noreply.github.com>
2024-09-13 21:51:11 -0400

    auto: Keyboard help deployment

---
+++ keyboard/sil_kayah_kali/1.3/sil_kayah_kali.php
@@ -0,0 +1,12 @@
+<?php
+  $pagename = 'Kayah [Kayah Li] (SIL) Keyboard Help';
+  $pagetitle = 'Kayah [Kayah Li] (SIL) Keyboard Help';
+  require_once('header.php');
+?>
+
+
+<p>This keyboard supports the Karenni (Kayah) language in the Kayah Li script.</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift rightalt rightalt-shift'>
+</div>
