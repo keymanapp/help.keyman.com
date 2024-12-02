b3e6b17892094a29384bb2445e3d250519f6feb1
Keyman Build Server <keyman-server@users.noreply.github.com>
2024-12-02 18:40:09 -0500

    auto: Keyboard help deployment

---
+++ keyboard/sil_lisu_standard/1.1.1/sil_lisu_standard.php
@@ -0,0 +1,12 @@
+<?php
+  $pagename = 'Lisu Standard (SIL) Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+
+  <p>This keyboard uses the standard Lisu keyboard layout.</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
