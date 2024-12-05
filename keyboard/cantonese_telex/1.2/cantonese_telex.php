5778fbcb19e50bb9afe6aeec6d04d74d5c52434a
Meng-Heng <havmengheng111@gmail.com>
2024-12-05 15:29:44 +0700

    pull to keyman-videos-website

---
+++ keyboard/cantonese_telex/1.2/cantonese_telex.php
@@ -0,0 +1,50 @@
+<?php 
+  $pagename = 'cantonese telex Keyboard Help';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+<p>
+    cantonese telex for Yụet Yữ Gwók Yữ Zị (Cantonese). Cantonese Telex粵語國語字輸入法
+<p>
+   <p> 粵語國語字係一種羅馬化嘅粵語文字 <p>
+      <p> Yuẹt Yữ Gwók Yữ Zị hậi yất zủng lò mã fá gé yuẹt yữ mần zị. <p>
+<p>
+   tone rules: 音調規則<p>
+   <p> 對應粵拼嘅第一聲唔使打，第二聲打r，第三聲打s, 第四聲打f，第五聲打x，第六聲打j <p>
+   <p> tone 2 = r, tone 3 =s, tone 4 = f, tone 5 = x, tone 6 = j <p>
+    examples: 舉例： <p>
+    <p> ar > ả, as > á, af > à, ax > ã, aj > ạ <p>
+    <p> 譬如話：‘梁’ 嘅羅馬字係‘lẻung’， 輸入內容係‘leungr’<p>
+    <img src='leung.png' alt='Keyboard Layout'>
+    <p> New Alphabet: 特別字母<p>
+    <p> a> â（元音短a), aa > a, oo > ô, ou > ô, uw > ư, ow > ơ, ee > ê <p>
+    <p> 雙擊音調鍵同特殊轉化鍵，取消轉化，變返原本嘅拼寫 <p>
+    <p> doubletaping leads to generate original letters:  <p>
+    <p> example:舉例：<p>
+    <p> fix = fixx, lax = laxx, bus=buss， hoo=hooo
+
+</p>
+
+<h1>音韻表 phonologic map</h1>
+<p> 建議以下嘅粵語國語字音韻表打字<p>
+
+<p><b>輔音consonants</b></p>
+<img src='consonants.png' alt='Keyboard Layout'>
+
+
+<p><b>元音vowels</b></p>
+<img src='vowels.png' alt='Keyboard Layout'>
+
+<p><b>音調tones</b></p>
+<img src='tones.png' alt='Keyboard Layout'>
+
+<!-- <p>See <a href="file:taigi_poj.pdf">this document</a> for all the key combinations.</p> -->
+
+<h1>Keyboard Layout</h1>
+
+<p><b>Unshifted</b></p>
+<img src='taigitelexU_.png' alt='Keyboard Layout'>
+<p><b>Shifted</b></p>
+<img src='taigitelexU_S.png' alt='Keyboard Layout'>
