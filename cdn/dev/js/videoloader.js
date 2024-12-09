bc52732d002208e57f783c964b4ae0246af75dc9
Meng-Heng <havmengheng111@gmail.com>
2024-12-09 12:45:57 +0700

    feat: applies Marc's suggestion to videoloader.js and classes

--- cdn/dev/js/videoloader.js
+++ cdn/dev/js/videoloader.js
@@ -1,13 +1,15 @@
-const thumbnailsList = document.querySelectorAll("#keymanThumbnails");
+( function() {
+  const thumbnailsList = document.querySelectorAll(".video-thumbnail");
 
-window.addEventListener('DOMContentLoaded', () => {
-  thumbnailsList.forEach(each => {
-  	each.addEventListener('click', () => {
-
-  		if(each.hasAttribute('data-video')) {
-        let videoId = each.getAttribute('data-video')
-    		each.outerHTML = "<iframe width='600' height='315' src='" + videoId + "' allowfullscreen></iframe>";
-      }
+  window.addEventListener('DOMContentLoaded', () => {
+    thumbnailsList.forEach(thumbnail => {
+      thumbnail.addEventListener('click', () => {
+  
+        if(thumbnail.hasAttribute('data-video')) {
+          let videoId = thumbnail.getAttribute('data-video')
+          thumbnail.outerHTML = "<iframe width='600' height='315' src='" + videoId + "' allowfullscreen></iframe>";
+        }
+      })
     })
   })
-})
+})();
