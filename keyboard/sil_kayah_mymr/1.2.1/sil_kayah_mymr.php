8d949bce0e1febc2fa9069b7863eee4357c3bf15
Keyman Build Server <keyman-server@users.noreply.github.com>
2025-01-02 14:09:15 -0500

    auto: Keyboard help deployment

---
+++ keyboard/sil_kayah_mymr/1.2.1/sil_kayah_mymr.php
@@ -0,0 +1,12 @@
+<?php
+  $pagename = 'Kayah [Myanmar] (SIL) Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+
+<p>This is a keyboard that contains all of the characters needed to type Kayah in the Myanmar script</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift'>
+</div>
