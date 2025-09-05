1e6becbbc5fa8fdbf63db4d77f4a8791a7ea2b9e
Eberhard Beilharz <ermshiperete@users.noreply.github.com>
2025-09-05 09:42:52 +0200

    Update _includes/includes/page-version-file-mappings.php
    Co-authored-by: Marc Durdin <marc@durdin.net>

--- _includes/includes/page-version-file-mappings.php
+++ _includes/includes/page-version-file-mappings.php
@@ -1,5 +1,7 @@
 <?php
-  // Mappings for files that got renamed at some point
+  // Mappings for files that got renamed at some point. These mappings are applied
+  // for the version switching mechanism on pages such as /developer/x.y/.... They
+  // are applied to _all_ versioned folders, so be careful when adding new mappings.
   global $PageVersionFileMappings;
   $PageVersionFileMappings = [
     ['whats-new', 'whatsnew'],
