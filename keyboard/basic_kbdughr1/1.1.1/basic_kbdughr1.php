1ddd91e871571b2b9d5eea16790654be7bdf8533
Keyman Build Server <keyman-server@users.noreply.github.com>
2026-03-10 15:09:59 -0500

    auto: Keyboard help deployment

---
+++ keyboard/basic_kbdughr1/1.1.1/basic_kbdughr1.php
@@ -0,0 +1,23 @@
+<?php 
+  $pagename = 'Uyghur Basic Keyboard Help';
+  $pagetitle = 'Uyghur Basic Keyboard Help';
+  // Header we will tidy up later  
+  require_once('header.php');
+
+
+?>
+
+
+<p>
+Uyghur Basic keyboard was imported with permission from the Windows 10 keyboard. This keyboard is designed for the Uyghur language of China using the Arabic script. 
+</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk'>
+</div>
+
+<h2>Mobile/Tablet Keyboard Layout</h2>
+<div id='osk-tablet' data-states='default shift'>
+</div>
+
+
