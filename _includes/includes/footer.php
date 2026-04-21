ef3a52306dcfc57a3e1da2867abd3d04f12f42a7
Darcy Wong <darcy_wong@sil.org>
2026-04-21 14:44:18 +0700

    chore: Update links about SIL Global

--- _includes/includes/footer.php
+++ _includes/includes/footer.php
@@ -51,8 +51,8 @@
         </div>
         <div class="footer-third sil-logo">
             <br>
-            <a href="https://www.keyman.com/about/"><img id="sil-logo" src="<?php echo ImageRandomizer::randomizer(); ?>" width="50%" alt='SIL' /></a>
-            <p>Created by <a href="https://www.keyman.com/about/">SIL Global</a></p>
+            <a href="https://global.sil.org/about/"><img id="sil-logo" src="<?php echo ImageRandomizer::randomizer(); ?>" width="50%" alt='SIL' /></a>
+            <p>Created by <a href="https://global.sil.org/about/">SIL Global</a></p>
         </div>
     </div>
 </div>
