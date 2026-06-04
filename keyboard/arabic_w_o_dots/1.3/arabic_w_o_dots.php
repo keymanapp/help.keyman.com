a03ed7c624f7ceafb4f0efe8ae9427da2a8a952b
Keyman Build Server <keyman-server@users.noreply.github.com>
2026-06-04 14:23:39 -0500

    auto: Keyboard help deployment

---
+++ keyboard/arabic_w_o_dots/1.3/arabic_w_o_dots.php
@@ -0,0 +1,31 @@
+<?php 
+  $pagename = 'Arabic W/O Dots Keyboard Help';
+  $pagetitle = $pagename;
+  require_once('header.php');
+?>
+
+
+<p>
+   Arabic Alphabet without the dots like the Olden Days 
+</p>
+
+<h2>Desktop Keyboard Layout</h2>
+<div id='osk' data-states='default shift alt alt-shift'>
+</div>
+
+<h2>Mobile/Phone Keyboard Layout</h2>
+
+<p style="flex-direction: row;">
+    <ul>Dotless Alphabets are in the Default Layout</ul>
+    <img src="typing-guide.jpg" alt="Arabic Mobile Layout">
+    <ul>Dotted alphabets are <b>Shift Up</b></ul>
+    <img src="typing-guide-shift.jpg" alt="Arabic Mobile Keyboard Shift">
+  </p>
+  <p style="flex-direction: row;">
+    <ul>Arabic Numerals are in the <b>Number Layout</b></ul>
+    <ul>Hindi Numerals are in Number Layout, <b>Shift Up</b></ul>
+    <img src="typing-guide-numbers.jpg" alt="Arabic Mobile Keyboard Numbers">
+  </p>
+  <p>
+    <img src="typing-guide-symbols.jpg" alt="Arabic Mobile Keyboard Symbols">
+  </p>
